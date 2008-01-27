805afe0f6437417389503f8112d555a4b8f2d372
doc_bacardi <devnull@localhost>
2008-01-27 17:41:49 +0000

    use platform specific config files for openocd

---
+++ plugins/romloader/openocd/openocd/config_msvc.h
@@ -0,0 +1,123 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* 0 if you don't want the Amontec JTAG-Accelerator driver. */
+//#define BUILD_AMTJTAGACCEL 0
+
+/* 0 if you don't want at91rm9200. */
+//#define BUILD_AT91RM9200 0
+
+/* 0 if you don't want a bitbang interface. */
+//#define BUILD_BITBANG 0
+
+/* 0 if you don't want a bitq interface. */
+//#define BUILD_BITQ 0
+
+/* 0 if you don't want ep93xx. */
+//#define BUILD_EP93XX 0
+
+/* 0 if you don't want ftd2xx ft2232. */
+#define BUILD_FT2232_FTD2XX 1
+
+/* 0 if you don't want libftdi ft2232. */
+//#define BUILD_FT2232_LIBFTDI 0
+
+/* 0 if you don't want the Gateworks GW16012 driver. */
+//#define BUILD_GW16012 0
+
+/* 0 if you don't want parport. */
+//#define BUILD_PARPORT 0
+
+/* 0 if you don't want the ASIX PRESTO driver. */
+//#define BUILD_PRESTO 0
+
+/* 0 if you don't want the usbprog JTAG driver. */
+//#define BUILD_USBPROG 0
+
+/* Define to 1 if you have the <elf.h> header file. */
+//#define HAVE_ELF_H 0
+
+/* Define to 1 if you have the `gettimeofday' function. */
+//#define HAVE_GETTIMEOFDAY 0
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+//#define HAVE_INTTYPES_H 0
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the `strnlen' function. */
+#define HAVE_STRNLEN 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+//#define HAVE_SYS_PARAM_H 0
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+//#define HAVE_UNISTD_H 0
+
+/* Define to 1 if you have the `usleep' function. */
+//#define HAVE_USLEEP 0
+
+/* 0 if not building for Cygwin. */
+//#define IS_CYGWIN 0
+
+/* 1 if building for MinGW. */
+/* #undef IS_MINGW */
+
+/* 0 if not building for Win32. */
+#define IS_WIN32 1
+
+/* Name of package */
+#define PACKAGE "openocd"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* 0 if you don't want parport to use giveio. */
+//#define PARPORT_USE_GIVEIO 0
+
+/* 0 if you don't want parport to use ppdev. */
+//#define PARPORT_USE_PPDEV 0
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
