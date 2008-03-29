2e76cba492c9c2c176ac2dfad40be03a4f9067f6
doc_bacardi <devnull@localhost>
2008-03-29 19:45:35 +0000

    windows fixes

--- gen_version.bat
+++ gen_version.bat
@@ -1,6 +1,6 @@
-m4 -E <application/templates/muhkuh_version.h.m4 >application/muhkuh_version.h.tmp
-cmp -s application/muhkuh_version.h.tmp application/muhkuh_version.h || cp application/muhkuh_version.h.tmp application/muhkuh_version.h
+m4 -E <application\templates\muhkuh_version.h.m4 >application\muhkuh_version.h.tmp
+cmp -s application\muhkuh_version.h.tmp application\muhkuh_version.h || copy application\muhkuh_version.h.tmp application\muhkuh_version.h
 
-m4 -E <installer/inno/templates/muhkuh.iss.m4 >installer/inno/muhkuh.iss.tmp
-cmp -s installer/inno/muhkuh.iss.tmp installer/inno/muhkuh.iss || cp installer/inno/muhkuh.iss.tmp installer/inno/muhkuh.iss
+m4 -E <installer\inno\templates\muhkuh.iss.m4 >installer\inno\muhkuh.iss.tmp
+cmp -s installer\inno\muhkuh.iss.tmp installer\inno\muhkuh.iss || copy installer\inno\muhkuh.iss.tmp installer\inno\muhkuh.iss
 
