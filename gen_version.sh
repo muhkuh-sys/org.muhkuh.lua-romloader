0161487dda02dbf3ad676dd9b48d4c46dcbee141
doc_bacardi <devnull@localhost>
2008-03-29 19:42:55 +0000

    added special linux version of gen_version

---
+++ gen_version.sh
@@ -0,0 +1,6 @@
+m4 -E <application/templates/muhkuh_version.h.m4 >application/muhkuh_version.h.tmp
+cmp -s application/muhkuh_version.h.tmp application/muhkuh_version.h || cp application/muhkuh_version.h.tmp application/muhkuh_version.h
+
+m4 -E <installer/inno/templates/muhkuh.iss.m4 >installer/inno/muhkuh.iss.tmp
+cmp -s installer/inno/muhkuh.iss.tmp installer/inno/muhkuh.iss || cp installer/inno/muhkuh.iss.tmp installer/inno/muhkuh.iss
+
