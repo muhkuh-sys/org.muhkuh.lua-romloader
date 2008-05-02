338610b01e5627f1003bb7c5d076717c93a3b0ac
doc_bacardi <devnull@localhost>
2008-05-02 16:53:01 +0000

    generate application ressource from template

--- gen_version.bat
+++ gen_version.bat
@@ -1,6 +1,6 @@
 m4 -E <application\templates\muhkuh_version.h.m4 >application\muhkuh_version.h.tmp
 cmp -s application\muhkuh_version.h.tmp application\muhkuh_version.h || copy application\muhkuh_version.h.tmp application\muhkuh_version.h
 
-m4 -E <installer\inno\templates\muhkuh.iss.m4 >installer\inno\muhkuh.iss.tmp
-cmp -s installer\inno\muhkuh.iss.tmp installer\inno\muhkuh.iss || copy installer\inno\muhkuh.iss.tmp installer\inno\muhkuh.iss
+m4 -E <application\templates\muhkuh.rc.m4 >application\muhkuh.rc.tmp
+cmp -s application\muhkuh.rc.tmp application\muhkuh.rc || copy application\muhkuh.rc.tmp application\muhkuh.rc
 
