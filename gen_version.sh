835885be21aafb128f637b88cfcff105e37eb2d5
doc_bacardi <devnull@localhost>
2008-05-04 21:16:31 +0000

    fixed gen_version script

--- gen_version.sh
+++ gen_version.sh
@@ -2,5 +2,5 @@ m4 -E <application/templates/muhkuh_version.h.m4 >application/muhkuh_version.h.t
 cmp -s application/muhkuh_version.h.tmp application/muhkuh_version.h || cp application/muhkuh_version.h.tmp application/muhkuh_version.h
 
 m4 -E <application/templates/muhkuh.rc.m4 >application/muhkuh.rc.tmp
-cmp -s application/muhkuh.rc.tmp application/muhkuh.rc || copy application/muhkuh.rc.tmp application/muhkuh.rc
+cmp -s application/muhkuh.rc.tmp application/muhkuh.rc || cp application/muhkuh.rc.tmp application/muhkuh.rc
 
