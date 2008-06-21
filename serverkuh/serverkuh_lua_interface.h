0c35246114478a3edf696d4d1df93827788df57e
doc_bacardi <devnull@localhost>
2008-06-21 14:24:40 +0000

    added server specific lua interface

---
+++ serverkuh/serverkuh_lua_interface.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Christoph Thelen                                *
+ *   doc_bacardi@users.sourceforge.net                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include <wx/wx.h>
+
+#include "muhkuh_plugin_manager.h"
+#include "muhkuh_wrap_xml.h"
+
+
+#ifndef __MUHKUH_LUA_INTERFACE_H__
+#define __MUHKUH_LUA_INTERFACE_H__
+
+
+void TestHasFinished(void);
+
+wxString load(wxString strFileName);
+void include(wxString strFileName);
+
+void setLogMarker(void);
+wxString getMarkedLog(void);
+
+void ScanPlugins(wxString strPattern);
+muhkuh_plugin_instance *GetNextPlugin(void);
+
+muhkuh_wrap_xml *GetSelectedTest();
+
+#endif	// __MUHKUH_LUA_INTERFACE_H__
+
