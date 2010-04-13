5bcc018d39d1cf5049f9f8f9a3cb8e19c27a8c7e
doc_bacardi <devnull@localhost>
2010-04-13 12:49:55 +0000

    added netx10 serial vectors

--- plugins/romloader/_test/testcode/src/serial_vectors.h
+++ plugins/romloader/_test/testcode/src/serial_vectors.h
@@ -22,11 +22,29 @@
 #ifndef __SERIAL_VECTORS_H__
 #define __SERIAL_VECTORS_H__
 
+#if ASIC_TYP==10
+typedef unsigned char	(*pfnSerialGet_t)	(unsigned int uiHandle);
+typedef void		(*pfnSerialPut_t)	(unsigned int uiHandle, unsigned int iChar);
+typedef unsigned int	(*pfnSerialPeek_t)	(unsigned int uiHandle);
+typedef void		(*pfnSerialFlush_t)	(unsigned int uiHandle);
 
+#define SERIAL_GET() tSerialVectors.fn.fnGet(0)
+#define SERIAL_PUT(c) tSerialVectors.fn.fnPut(0,c)
+#define SERIAL_PEEK() tSerialVectors.fn.fnPeek(0)
+#define SERIAL_FLUSH() tSerialVectors.fn.fnFlush(0)
+
+#else
 typedef unsigned char	(*pfnSerialGet_t)	(void);
 typedef void		(*pfnSerialPut_t)	(unsigned int iChar);
 typedef unsigned int	(*pfnSerialPeek_t)	(void);
 typedef void		(*pfnSerialFlush_t)	(void);
+
+#define SERIAL_GET() tSerialVectors.fn.fnGet()
+#define SERIAL_PUT(c) tSerialVectors.fn.fnPut(c)
+#define SERIAL_PEEK() tSerialVectors.fn.fnPeek()
+#define SERIAL_FLUSH() tSerialVectors.fn.fnFlush()
+
+#endif
 
 typedef struct
 {
@@ -42,7 +60,7 @@ typedef union
 	unsigned long aul[4];
 } SerialCommUiFn_t;
 
-extern SerialCommUiFn_t serial_vectors;
+extern SerialCommUiFn_t tSerialVectors;
 
 #endif	/* __SERIAL_VECTORS_H__ */
 
