8257bbc26e477c66ad16570523bd635636937050
doc_bacardi <devnull@localhost>
2010-12-09 11:40:00 +0000

    replace m4 with cmake templates

---
+++ application/templates/muhkuh_version.h
@@ -0,0 +1,13 @@
+
+#ifndef __muhkuh_version_h__
+#define __muhkuh_version_h__
+
+#define MUHKUH_APPLICATION_NAME "Muhkuh"
+#define MUHKUH_VERSION_MAJ @MUHKUH_VERSION_MAJ@
+#define MUHKUH_VERSION_MIN @MUHKUH_VERSION_MIN@
+#define MUHKUH_VERSION_SUB @MUHKUH_VERSION_SUB@
+#define MUHKUH_VERSION_YEAR "@MUHKUH_VERSION_YEAR@"
+
+#define MUHKUH_VERSION_STRING "@MUHKUH_VERSION_MAJ@.@MUHKUH_VERSION_MIN@.@MUHKUH_VERSION_SUB@"
+
+#endif	/* __muhkuh_version_h__ */
