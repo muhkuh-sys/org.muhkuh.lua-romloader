5080b0a2118ff8af5ffbd240008d021502104887
doc_bacardi <devnull@localhost>
2011-04-27 14:40:10 +0000

    fix parameter setting

--- plugins/romloader/uart/netx/testbench/test_romloader_call.lua
+++ plugins/romloader/uart/netx/testbench/test_romloader_call.lua
@@ -24,18 +24,20 @@ else
 	error("Unknown chiptyp:", tPlugin)
 end
 
+ulParameter = 0x12345678
+
 aAttr = tester.mbin_open(strFilename)
 tester.mbin_debug(aAttr)
 tester.mbin_write(nil, tPlugin, aAttr)
-tester.mbin_set_parameter(tPlugin, aAttr, aParameter)
+tester.mbin_set_parameter(tPlugin, aAttr, ulParameter)
 ulValue = tester.mbin_execute(nil, tPlugin, aAttr)
 print(string.format("Returncode: 0x%08x", ulValue))
 if ulValue~=0 then
-	error("The code returned an error:", ulValue)
+	error(string.format("The code returned an error: 0x%08x", ulValue))
 end
 
 ulValue = tPlugin:read_data32(aAttr.ulParameterStartAddress+8)
 print(string.format("Return Parameter: 0x%08x", ulValue))
-if ulValue~=0x12345678 then
-	error("The code did not copy the input parameter. Expected 0x12345678, got:", ulValue)
+if ulValue~=ulParameter then
+	error(string.format("The code did not copy the input parameter. Expected 0x%08x, got 0x%08x", ulParameter, ulValue))
 end
