d6df90696a317807dc8e51a408dc6fc55bbe6c42
doc_bacardi <devnull@localhost>
2011-04-29 14:00:22 +0000

    fix missing bytes after card change, started identify loader

--- plugins/romloader/uart/romloader_uart_device.cpp
+++ plugins/romloader/uart/romloader_uart_device.cpp
@@ -120,6 +120,8 @@ void romloader_uart_device::writeCards(const unsigned char *pucBuffer, size_t si
 		// no more space -> create a new card
 		if( sizChunk==0 )
 		{
+			fprintf(stderr, "New Card\n");
+
 			ptCard = new tBufferCard;
 			ptCard->pucEnd = ptCard->aucData + mc_sizCardSize;
 			ptCard->pucRead = ptCard->aucData;
@@ -158,93 +160,94 @@ void romloader_uart_device::writeCards(const unsigned char *pucBuffer, size_t si
 }
 
 
-size_t romloader_uart_device::readCards(unsigned char *pucBuffer, size_t sizBufferSize)
+size_t romloader_uart_device::readCards(unsigned char *pucBuffer, size_t sizBuffer)
 {
-	size_t sizRead;
 	size_t sizLeft;
-
-
-	sizLeft = sizBufferSize;
-	do
-	{
-		sizRead = readCardData(pucBuffer, sizLeft);
-		pucBuffer += sizRead;
-		sizLeft -= sizRead;
-	} while( sizRead!=0 && sizLeft>0 );
-
-	return sizBufferSize-sizLeft;
-}
-
-
-size_t romloader_uart_device::readCardData(unsigned char *pucBuffer, size_t sizBufferSize)
-{
 	size_t sizRead;
 	tBufferCard *ptOldCard;
 	tBufferCard *ptNewCard;
 
 
-	if( m_ptFirstCard==NULL )
+	sizLeft = sizBuffer;
+	do
 	{
-		sizRead = 0;
-	}
-	else if( m_ptFirstCard->pucWrite!=NULL )
-	{
-		// the first card is used by the write part -> lock the cards
-		CRITICAL_SECTION_ENTER(m_csCardLock);
+		if( m_ptFirstCard==NULL )
+		{
+			sizRead = 0;
+			break;
+		}
+		else if( m_ptFirstCard->pucWrite!=NULL )
+		{
+			/* The first card is used by the write part -> lock the cards. */
+			CRITICAL_SECTION_ENTER(m_csCardLock);
 
-		// get the number of bytes left in this card
-		sizRead = m_ptFirstCard->pucWrite - m_ptFirstCard->pucRead;
-		if( sizRead>sizBufferSize )
-		{
-			sizRead = sizBufferSize;
-		}
-		// card can be empty
-		if( sizRead>0 )
-		{
-			// copy the data
-			memcpy(pucBuffer, m_ptFirstCard->pucRead, sizRead);
-			// advance the read pointer
-			m_ptFirstCard->pucRead += sizRead;
-		}
-
-		CRITICAL_SECTION_LEAVE(m_csCardLock);
-	}
-	else
-	{
-		// the first card is not used by the write part
-
-		// get the number of bytes left in this card
-		sizRead = m_ptFirstCard->pucEnd - m_ptFirstCard->pucRead;
-		if( sizRead>sizBufferSize )
-		{
-			sizRead = sizBufferSize;
-		}
-		// card can be empty for overlapping buffer grow
-		if( sizRead>0 )
-		{
-			// copy the data
-			memcpy(pucBuffer, m_ptFirstCard->pucRead, sizRead);
-			// advance the read pointer
-			m_ptFirstCard->pucRead += sizRead;
-		}
-		// reached the end of the buffer?
-		if( m_ptFirstCard->pucRead>=m_ptFirstCard->pucEnd )
-		{
-			// card is empty, move on to next card
-			ptNewCard = m_ptFirstCard->ptNext;
-			if( ptNewCard!=NULL )
+			/* Get the number of bytes left in this card. */
+			sizRead = m_ptFirstCard->pucWrite - m_ptFirstCard->pucRead;
+			if( sizRead>sizLeft )
 			{
-				// remember the empty card
-				ptOldCard = m_ptFirstCard;
-				// move to the new first card
-				m_ptFirstCard = ptNewCard;
-				// delete the empty card
-				delete ptOldCard;
+				sizRead = sizLeft;
+			}
+			/* Card can be empty. */
+			if( sizRead>0 )
+			{
+				/* Copy the data. */
+				memcpy(pucBuffer, m_ptFirstCard->pucRead, sizRead);
+				/* Advance the read pointer. */
+				m_ptFirstCard->pucRead += sizRead;
+			}
+
+			CRITICAL_SECTION_LEAVE(m_csCardLock);
+
+			if( sizRead==0 )
+			{
+				/* No more data available. */
+				break;
 			}
 		}
-	}
+		else
+		{
+			// the first card is not used by the write part
 
-	return sizRead;
+			// get the number of bytes left in this card
+			sizRead = m_ptFirstCard->pucEnd - m_ptFirstCard->pucRead;
+			if( sizRead>sizLeft )
+			{
+				sizRead = sizLeft;
+			}
+			// card can be empty for overlapping buffer grow
+			if( sizRead>0 )
+			{
+				// copy the data
+				memcpy(pucBuffer, m_ptFirstCard->pucRead, sizRead);
+				// advance the read pointer
+				m_ptFirstCard->pucRead += sizRead;
+			}
+			else
+			{
+				fprintf(stderr, "WARNING: no remaining data: %d\n", sizRead);
+			}
+			// reached the end of the buffer?
+			if( m_ptFirstCard->pucRead>=m_ptFirstCard->pucEnd )
+			{
+				// card is empty, move on to next card
+				ptNewCard = m_ptFirstCard->ptNext;
+				if( ptNewCard!=NULL )
+				{
+					// remember the empty card
+					ptOldCard = m_ptFirstCard;
+					// move to the new first card
+					m_ptFirstCard = ptNewCard;
+					// delete the empty card
+					delete ptOldCard;
+				}
+			}
+		}
+
+		sizLeft -= sizRead;
+		pucBuffer += sizRead;
+	} while( sizLeft>0 );
+
+	return sizBuffer-sizLeft;
 }
 
 
@@ -285,19 +288,128 @@ unsigned int romloader_uart_device::crc16(unsigned short usCrc, unsigned char uc
 }
 
 
+bool romloader_uart_device::GetLine(unsigned char **ppucLine, const char *pcEol, unsigned long ulTimeout)
+{
+	bool fOk;
+	unsigned char *pucBuffer;
+	unsigned char *pucBufferNew;
+	size_t sizBufferCnt;
+	size_t sizBufferMax;
+	size_t sizEolSeqCnt;
+	size_t sizEolSeqMax;
+	size_t sizReceived;
+
+
+	/* Receive char by char until the EOL sequence was received. */
+
+	/* Expect success. */
+	fOk = true;
+
+	sizEolSeqCnt = 0;
+	sizEolSeqMax = strlen(pcEol);
+
+	/* Init References array. */
+	sizBufferCnt = 0;
+	sizBufferMax = 80;
+	pucBuffer = (unsigned char*)malloc(sizBufferMax);
+	if( pucBuffer==NULL )
+	{
+		fprintf(stderr, "out of memory!\n");
+		fOk = false;
+	}
+	else
+	{
+		do
+		{
+			sizReceived = RecvRaw(pucBuffer+sizBufferCnt, 1, ulTimeout);
+			if( sizReceived!=1 )
+			{
+				/* Timeout, that's it... */
+				fOk = false;
+				break;
+			}
+			else
+			{
+				/* Check for EOL. */
+				if( pcEol[sizEolSeqCnt]==pucBuffer[sizBufferCnt] )
+				{
+					++sizEolSeqCnt;
+				}
+				else
+				{
+					sizEolSeqCnt = 0;
+				}
+			}
+
+			/* Is enough space in the array for one more entry? */
+			/* NOTE: Do this at the end of the loop because the line is
+			terminated with a '\0'. */
+			if( sizBufferCnt>=sizBufferMax )
+			{
+				/* No -> expand the array. */
+				sizBufferMax *= 2;
+				/* Detect overflow or limitation. */
+				if( sizBufferMax<=sizBufferCnt )
+				{
+					fOk = false;
+					break;
+				}
+				/* Reallocate the array. */
+				pucBufferNew = (unsigned char*)realloc(pucBuffer, sizBufferMax);
+				if( pucBufferNew==NULL )
+				{
+					fOk = false;
+					break;
+				}
+				pucBuffer = pucBufferNew;
+			}
+		} while( sizEolSeqCnt<sizEolSeqMax );
+	}
+
+	if( fOk==true )
+	{
+		/* Terminate the line buffer. */
+		pucBuffer[sizBufferCnt] = 0;
+	}
+	else if( pucBuffer!=NULL )
+	{
+		free(pucBuffer);
+		pucBuffer = NULL;
+	}
+
+	*ppucLine = pucBuffer;
+
+	return fOk;
+}
+
+
+const char *romloader_uart_device::apcRomcodeWelcomeStrings[3] =
+{
+	"netX500 Step A\r\n",
+	"netX50 Step B\r\n",
+	"netX10 Step B\r\n"
+};
+
 bool romloader_uart_device::IdentifyLoader(void)
 {
 	bool fResult = false;
-	//wxRegEx reFirstResponse(wxT("netX.* Step [A-Za-z0-9]"));
-	//wxRegEx reInvalidCmd(wxT(".*\\?[0-9]{8}[\r\n]*>"));
-	const unsigned char aucKnock[2] = { '*', '#' };
-	unsigned long ulLength;
-	unsigned char aucData[1];
+	const unsigned char aucKnock[5] = { '*', 0x00, 0x00, '*', '#' };
+	const unsigned char aucKnockResponseMi[7] = { 0x09, 0x00, 0x00, 0x4d, 0x4f, 0x4f, 0x48 };
+	unsigned char aucData[13];
 	unsigned char *pucResponse;
+	size_t sizCnt;
+	unsigned long ulMiVersionMaj;
+	unsigned long ulMiVersionMin;
+	bool fDeviceNeedsUpdate;
+	unsigned short usCrc;
+	const unsigned char aucBlankLine[1] = { '\n' };
 
 
-	/* Send knock sequence with 1 second timeout. */
-	if( SendRaw(aucKnock, sizeof(aucKnock), 1000)!=sizeof(aucKnock) )
+	/* No update by default. */
+	fDeviceNeedsUpdate = false;
+
+	/* Send knock sequence with 500ms second timeout. */
+	if( SendRaw(aucKnock, sizeof(aucKnock), 500)!=sizeof(aucKnock) )
 	{
 		/* Failed to send knock sequence to device. */
 		fprintf(stderr, "Failed to send knock sequence to device.\n");
@@ -309,86 +421,120 @@ bool romloader_uart_device::IdentifyLoader(void)
 	}
 	else
 	{
-		ulLength = RecvRaw(aucData, 1, 5000);
-	 	if( ulLength!=1 )
-	 	{
+		sizCnt = RecvRaw(aucData, 1, 1000);
+		if( sizCnt!=1 )
+		{
 			/* Failed to receive first char of knock response. */
 			fprintf(stderr, "Failed to receive first char of knock response.\n");
 		}
 		else
 		{
-			// wxLogMessage(wxT("received knock response: 0x%02x"), ucData);
-			printf("received knock response: 0x%02x", aucData);
+			printf("received knock response: 0x%02x\n", aucData[0]);
 			/* This should be '\f', but the first bits might be trashed. */
 			if( aucData[0]<0x20 )
 			{
 				/* This seems to be the welcome message. */
-#if 0
-				// receive the rest of the line
-				//wxLogMessage(wxT("receive the rest of the knock response"));
-				fResult = GetLine(&pucResponse, "\r\n", 1000);
+
+				/* Receive the rest of the line. */
+				printf("receive the rest of the knock response\n");
+				fResult = GetLine(&pucResponse, "\r\n", 200);
 				if( fResult!=true )
 				{
-					// failed to receive the rest of the knock response
+					fprintf(stderr, "Failed to receive the rest of the knock response!\n");
 				}
 				else
 				{
-					// check for initial bootloader message
-					fResult = reFirstResponse.Matches(pucResponse);
-					if( fResult!=true )
+					fResult = false;
+					for(sizCnt=0; sizCnt<(sizeof(apcRomcodeWelcomeStrings)/sizeof(const char**)); ++sizCnt)
 					{
-						// seems to be no netX bootloader
-						//wxLogMessage(wxT("the knock response does not match the romloader message"));
-						//wxLogMessage(wxT("received this response: '") + strResponse + wxT("'"));
+						if( strcmp((const char*)pucResponse, apcRomcodeWelcomeStrings[sizCnt])==0 )
+						{
+							fResult = true;
+							break;
+						}
+					}
+					/* Check for initial bootloader message. */
+					if( fResult==false )
+					{
+						/* Seems to be no netX bootloader. */
+						fprintf(stderr, "Received invalid response from the device: '%s'\n", pucResponse);
+						free(pucResponse);
 					}
 					else
 					{
-						// get prompt, the real console eats the first space, the usb console will echo it
+						free(pucResponse);
+
+						/* Get prompt, the real console eats the first char, the usb console will echo it. */
 						fResult = false;
-						ulLength = RecvRaw(abData, 3, 1000);
-						if( ulLength==0 )
+						sizCnt = RecvRaw(aucData, 3, 400);
+						if( sizCnt==0 )
 						{
-							//wxLogMessage(wxT("received no further data after romloader message"));
+							printf("received no further data after romloader message.\n");
 						}
-						else if( abData[0]!='>' )
+						else if( aucData[0]!='>' )
 						{
-							//wxLogMessage(wxT("received strange response after romloader message"));
-							//wxLogMessage(wxT("len: %d, data = 0x%02x, 0x%02x, 0x%02x"), ulLength, abData[0], abData[1], abData[2]);
+							fprintf(stderr, "received strange response after romloader message!\n");
+							fprintf(stderr, "len: %d, data = 0x%02x, 0x%02x, 0x%02x\n", sizCnt, aucData[0], aucData[1], aucData[2]);
 						}
-						else if( ulLength==1 )
+						else if( sizCnt==1 )
 						{
-							//wxLogMessage(wxT("ok, received prompt!"));
+							printf("ok, received prompt!\n");
+							fDeviceNeedsUpdate = true;
 							fResult = true;
 						}
-						else if( ulLength==2 && abData[1]=='#' )
+						else if( sizCnt==2 && aucData[1]=='#' )
 						{
-							//wxLogMessage(wxT("ok, received hash!"));
+							printf("ok, received hash!\n");
+							fDeviceNeedsUpdate = true;
 							fResult = true;
 						}
-						else if( ulLength==3 && abData[1]=='*' && abData[2]=='#' )
+						else if( sizCnt==3 && aucData[1]=='*' && aucData[2]=='#' )
 						{
-							//wxLogMessage(wxT("ok, received star and hash!"));
+							printf("ok, received star and hash!");
+							fDeviceNeedsUpdate = true;
 							fResult = true;
 						}
 						else
 						{
-							//wxLogMessage(wxT("received strange response after romloader message"));
-							//wxLogMessage(wxT("len: %d, data = 0x%02x, 0x%02x, 0x%02x"), ulLength, abData[0], abData[1], abData[2]);
+							fprintf(stderr, "received strange response after romloader message!\n");
+							printf("len: %d, data = 0x%02x, 0x%02x, 0x%02x", sizCnt, aucData[0], aucData[1], aucData[2]);
 						}
 					}
 				}
-#endif
 			}
-			/* Knock echoed -> this is the prompt. */
+			/* Knock echoed -> this is the prompt or the machine interface. */
 			else if( aucData[0]=='*' )
 			{
 				printf("ok, received star!\n");
 
-				// get rest of knock sequence
-				ulLength = RecvRaw(aucData, 1, 1000);
-				if( ulLength==0 )
+				/* Get rest of the response. */
+				sizCnt = RecvRaw(aucData, 13, 500);
+				hexdump(aucData, sizCnt);
+				printf("%d\n", sizCnt);
+				hexdump(aucKnockResponseMi, 7);
+				if( sizCnt==0 )
 				{
-					printf("failed to receive response after the star\n");
+					fprintf(stderr, "Failed to receive response after the star!\n");
+				}
+				else if( sizCnt==13 && memcmp(aucData, aucKnockResponseMi, 7)==0 )
+				{
+					/* Build the crc for the packet. */
+					usCrc = 0;
+					for(sizCnt==0; sizCnt<13; ++sizCnt)
+					{
+						usCrc = crc16(usCrc, aucData[sizCnt]);
+					}
+					if( usCrc!=0 )
+					{
+						fprintf(stderr, "Crc of version packet is invalid!\n");
+					}
+					else
+					{
+						ulMiVersionMin = aucData[7] | (aucData[8]<<8);;
+						ulMiVersionMaj = aucData[9] | (aucData[10]<<8);
+						printf("Found new machine interface V%d.%d.\n", ulMiVersionMaj, ulMiVersionMin);
+						fResult = true;
+					}
 				}
 				else if( aucData[0]!='#' )
 				{
@@ -397,6 +543,7 @@ bool romloader_uart_device::IdentifyLoader(void)
 				else
 				{
 					printf("ok, received hash!\n");
+					fDeviceNeedsUpdate = true;
 					fResult = true;
 				}
 			}
@@ -404,6 +551,7 @@ bool romloader_uart_device::IdentifyLoader(void)
 			{
 				printf("ok, received hash!\n");
 
+				fDeviceNeedsUpdate = true;
 				fResult = true;
 			}
 			else
@@ -415,30 +563,67 @@ bool romloader_uart_device::IdentifyLoader(void)
 
 	if( fResult==true )
 	{
-#if 0
-		/* Send enter. */
-		printf("sending enter to netx");
-		fResult = SendCommand("", 1000);
-		if( fResult!=true )
+		if( fDeviceNeedsUpdate==true )
 		{
-			//wxLogError(wxT("failed to send enter to device"));
-		}
-		else
-		{
-			fResult = WaitForResponse(&pucResponse, 65536, 1024);
-			if( fResult!=true )
+			/* Send enter. */
+			printf("sending enter to netx.\n");
+			sizCnt = SendRaw(aucBlankLine, sizeof(aucBlankLine), 200);
+			if( sizCnt!=1 )
 			{
-				//wxLogMessage(wxT("failed to receive response after enter"));
+				fprintf(stderr, "Failed to send enter to device!\n");
 			}
 			else
 			{
-				//wxLogMessage(wxT("knock response:") + strResponse);
+//				fResult = WaitForResponse(&pucResponse, 65536, 1024);
+/* TODO:
+ 1) grab data until next newline + '>' Combo.
+ 2) download new code
+ 3) start
+*/
+
 			}
 		}
-#endif
 	}
 
 
 	return fResult;
 }
 
+
+void romloader_uart_device::hexdump(const unsigned char *pucData, unsigned long ulSize)
+{
+	const unsigned char *pucDumpCnt, *pucDumpEnd;
+	unsigned long ulAddressCnt;
+	size_t sizBytesLeft;
+	size_t sizChunkSize;
+	size_t sizChunkCnt;
+
+
+	// show a hexdump of the data
+	pucDumpCnt = pucData;
+	pucDumpEnd = pucData + ulSize;
+	ulAddressCnt = 0;
+	while( pucDumpCnt<pucDumpEnd )
+	{
+		// get number of bytes for the next line
+		sizChunkSize = 16;
+		sizBytesLeft = pucDumpEnd - pucDumpCnt;
+		if( sizChunkSize>sizBytesLeft )
+		{
+			sizChunkSize = sizBytesLeft;
+		}
+
+		// start a line in the dump with the address
+		printf("%08lX: ", ulAddressCnt);
+		// append the data bytes
+		sizChunkCnt = sizChunkSize;
+		while( sizChunkCnt!=0 )
+		{
+			printf("%02X ", *(pucDumpCnt++));
+			--sizChunkCnt;
+		}
+		// next line
+		printf("\n");
+		ulAddressCnt += sizChunkSize;
+	}
+}
