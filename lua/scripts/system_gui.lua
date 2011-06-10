055b55eee913caaba45b8bbb26014a958097db73
doc_bacardi <devnull@localhost>
2011-06-10 07:39:22 +0000

    fix init order, add missing plugin

--- lua/scripts/system_gui.lua
+++ lua/scripts/system_gui.lua
@@ -1,24 +1,29 @@
--- Load all system modules.
-require("bit")
-require("muhkuh")
-require("muhkuh_system")
-require("wx")
-
--- Load all enabled plugins.
-require("romloader_usb")
-
 -- Add all include paths.
-package.path  = package.path  .. ";/home/cthelen/Compile/muhkuh_experimental/bin/lua/?.lua"
--- package.cpath = package.cpath + "";
+package.path  = package.path  .. ";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_modules/?.lua"
+
+-- Add all search paths for plugins.
+package.cpath = package.cpath .. ";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_plugins/?.so"
 
 -- Set the Muhkuh version.
 _G.__MUHKUH_VERSION = "Muhkuh V2.0.0"
 
 
-_G.__MUHKUH_TEST_XML = "file:/home/cthelen/Compile/muhkuh_experimental/build/build/romloader_test.mtd#zip:test_description.xml"
+_G.__MUHKUH_TEST_XML = "file:/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/data/test_description.xml"
 _G.__MUHKUH_TEST_INDEX = 0
 
--- add support for old style bitlib
+
+-- Load all system modules.
+require("bit")
+require("muhkuh")
+require("muhkuh_system")
+require("select_plugin_gui")
+require("wx")
+
+-- Load all enabled plugins.
+require("romloader_usb")
+
+
+-- Add support for old style bitlib.
 if not bit.bshl then
 	bit.bshl = bit.lshift
 end
@@ -26,8 +31,8 @@ if not bit.bshr then
 	bit.bshr = bit.rshift
 end
 
+-- NOTE: move this to a better place.
 function muhkuh.TestHasFinished()
---	wx.wxGetApp():GetTopWindow():Close()
 	wx.wxGetApp():ExitMainLoop()
 end
 
