74a70296729213d3c0162dbcbf791262562f6982
doc_bacardi <devnull@localhost>
2011-06-16 15:03:23 +0000

    turn script into generator

--- lua/scripts/system_gui.lua
+++ lua/scripts/system_gui.lua
@@ -1,40 +1,75 @@
--- Add all include paths.
-package.path  = package.path  .. ";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_modules/?.lua"
+aLuaModules = {
+	"bit",
+	"muhkuh",
+	"muhkuh_system",
+	"select_plugin_gui",
+	"wx"
+}
 
--- Add all search paths for plugins.
-package.cpath = package.cpath .. ";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_plugins/?.so"
+aLuaPlugins = {
+	"romloader_uart"
+	"romloader_usb"
+}
 
--- Set the Muhkuh version.
-_G.__MUHKUH_VERSION = "Muhkuh V2.0.0"
+print(muhkuh_app.plugin_count())
 
+aCode = {}
 
-_G.__MUHKUH_TEST_XML = "file:/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/data/test_description.xml"
-_G.__MUHKUH_TEST_INDEX = 0
-
-
--- Load all system modules.
-require("bit")
-require("muhkuh")
-require("muhkuh_system")
-require("select_plugin_gui")
-require("wx")
-
--- Load all enabled plugins.
-require("romloader_usb")
+table.insert(aCode, "-- Add all include paths.")
+table.insert(aCode, "package.path  = package.path  .. \";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_modules/?.lua\"")
+table.insert(aCode, "")
 
 
--- Add support for old style bitlib.
-if not bit.bshl then
-	bit.bshl = bit.lshift
+table.insert(aCode, "-- Add all search paths for plugins.")
+table.insert(aCode, "package.cpath = package.cpath .. \";/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/system_folder_is_somewhere_else/lua_plugins/?.so\"")
+table.insert(aCode, "")
+
+
+table.insert(aCode, "-- Set the Muhkuh version.")
+-- table.insert(aCode, "_G.__MUHKUH_VERSION = \"" .. muhkuh_app.get_version() .. "\"")
+table.insert(aCode, "_G.__MUHKUH_VERSION = \"Muhkuh 2.0.1234\"")
+table.insert(aCode, "")
+table.insert(aCode, "")
+
+
+table.insert(aCode, "_G.__MUHKUH_TEST_XML = \"file:/home/cthelen/Compile/muhkuh_experimental/build/build/demo_layout/data/test_description.xml\"")
+table.insert(aCode, "_G.__MUHKUH_TEST_INDEX = 0")
+table.insert(aCode, "")
+table.insert(aCode, "")
+
+
+table.insert(aCode, "-- Load all system modules.")
+for tKey,strName in pairs(aLuaModules) do
+	table.insert(aCode, string.format("require(\"%s\")", strName))
 end
-if not bit.bshr then
-	bit.bshr = bit.rshift
-end
+table.insert(aCode, "")
 
--- NOTE: move this to a better place.
-function muhkuh.TestHasFinished()
-	wx.wxGetApp():ExitMainLoop()
+table.insert(aCode, "-- Load all enabled plugins.")
+for tKey,strName in pairs(aLuaPlugins) do
+	table.insert(aCode, string.format("require(\"%s\")", strName))
 end
+table.insert(aCode, "")
+table.insert(aCode, "")
 
-muhkuh_system.boot_xml()
+
+table.insert(aCode, "-- Add support for old style bitlib.")
+table.insert(aCode, "if not bit.bshl then")
+table.insert(aCode, "\tbit.bshl = bit.lshift")
+table.insert(aCode, "end")
+table.insert(aCode, "if not bit.bshr then")
+table.insert(aCode, "\tbit.bshr = bit.rshift")
+table.insert(aCode, "end")
+table.insert(aCode, "")
+
+
+table.insert(aCode, "-- NOTE: move this to a better place.")
+table.insert(aCode, "function muhkuh.TestHasFinished()")
+table.insert(aCode, "\twx.wxGetApp():ExitMainLoop()")
+table.insert(aCode, "end")
+table.insert(aCode, "")
+
+
+table.insert(aCode, "muhkuh_system.boot_xml()")
+
+return table.concat(aCode, "\n")
 
