64491363bdf1de02b13eade9c9c28120abc5713a
doc_bacardi <devnull@localhost>
2012-08-22 17:31:59 +0200

    remove unused V1 vectors from bridge

--- plugins/romloader/machine_interface/netx/src/serial_vectors_bridge.h
+++ plugins/romloader/machine_interface/netx/src/serial_vectors_bridge.h
@@ -22,49 +22,6 @@
 #ifndef __SERIAL_VECTORS_BRIDGE_H__
 #define __SERIAL_VECTORS_BRIDGE_H__
 
-/* The V1 vectors are the interface to the hardware. They send data to the PC
-   and receive data from there.
-*/
-
-#if ASIC_TYP==10
-	typedef unsigned int (*PFN_SERIAL_V1_GET_T)(unsigned int tHandle);
-	typedef void         (*PFN_SERIAL_V1_PUT_T)(unsigned int tHandle, unsigned char ucChar);
-	typedef unsigned int (*PFN_SERIAL_V1_PEEK_T)(unsigned int tHandle);
-	typedef void         (*PFN_SERIAL_V1_FLUSH_T)(unsigned int tHandle);
-
-#       define SERIAL_V1_GET() tSerialV1Vectors.fn.fnGet(0)
-#       define SERIAL_V1_PUT(c) tSerialV1Vectors.fn.fnPut(0, c)
-#       define SERIAL_V1_PEEK() tSerialV1Vectors.fn.fnPeek(0)
-#       define SERIAL_V1_FLUSH() tSerialV1Vectors.fn.fnFlush(0)
-#else
-	typedef unsigned char (*PFN_SERIAL_V1_GET_T)(void);
-	typedef void          (*PFN_SERIAL_V1_PUT_T)(unsigned int iChar);
-	typedef unsigned int  (*PFN_SERIAL_V1_PEEK_T)(void);
-	typedef void          (*PFN_SERIAL_V1_FLUSH_T)(void);
-
-#       define SERIAL_V1_GET() tSerialV1Vectors.fn.fnGet()
-#       define SERIAL_V1_PUT(c) tSerialV1Vectors.fn.fnPut(c)
-#       define SERIAL_V1_PEEK() tSerialV1Vectors.fn.fnPeek()
-#       define SERIAL_V1_FLUSH() tSerialV1Vectors.fn.fnFlush()
-#endif
-
-typedef struct
-{
-	PFN_SERIAL_V1_GET_T fnGet;
-	PFN_SERIAL_V1_PUT_T fnPut;
-	PFN_SERIAL_V1_PEEK_T fnPeek;
-	PFN_SERIAL_V1_FLUSH_T fnFlush;
-} SERIAL_V1_COMM_FN_T;
-
-typedef union
-{
-	SERIAL_V1_COMM_FN_T fn;
-	unsigned long aul[4];
-} SERIAL_V1_COMM_UI_FN_T;
-extern SERIAL_V1_COMM_UI_FN_T tSerialV1Vectors;
-
-
-
 /* The V2 vectors are the interface to a program executed with the 'call'
    command.
 */
