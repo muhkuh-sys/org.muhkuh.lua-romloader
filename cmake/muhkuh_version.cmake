e88625b7ea00ed6a354f52ab8e69c3c049662674
doc_bacardi <devnull@localhost>
2013-10-18 18:37:52 +0200

    Change version handling: read a fixed major.minor.sub from muhkuh_version.txt and get MUHKUH_VERSION_VCS from "hg id -i" or an .hg_archival file.

--- cmake/muhkuh_version.cmake
+++ cmake/muhkuh_version.cmake
@@ -5,48 +5,63 @@
 
 # Set the filename for the muhkuh version file.
 SET(MUHKUH_VERSION_FILE "${CMAKE_HOME_DIRECTORY}/muhkuh_version.txt")
-IF(EXISTS ${MUHKUH_VERSION_FILE})
-	# Set the regular expression for the muhkuh version. It is a sequence of 2 numbers and a hash separated by dots.
-	SET(MUHKUH_VERSION_REXEX "([0-9]+)\\.([0-9]+)\\.([0-9a-fA-F]+)")
-	# Read the contents of the muhkuh version file. Accept only data matching the version regex.
-	FILE(STRINGS ${MUHKUH_VERSION_FILE} MUHKUH_VERSION_ALL REGEX ${MUHKUH_VERSION_REXEX})
-	# Check if a valid version number was found.
-	IF("${MUHKUH_VERSION_ALL}" STREQUAL "")
+
+# Set the regular expression for the muhkuh version. It is a sequence of 3 numbers separated by dots.
+SET(MUHKUH_VERSION_REGEX "([0-9]+)\\.([0-9]+)\\.([0-9]+)")
+
+# Set the regular expression for the ID in the .hg_archival.txt file.
+SET(HG_ARCHIVAL_VERSION_REGEX "node: ([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])[0-9a-fA-F]+")
+
+# Read the contents of the muhkuh version file. Accept only data matching the version regex.
+FILE(STRINGS ${MUHKUH_VERSION_FILE} MUHKUH_VERSION_ALL REGEX ${MUHKUH_VERSION_REGEX})
+
+# Check if a valid version number was found.
+IF("${MUHKUH_VERSION_ALL}" STREQUAL "")
+	# No valid version number found. The file contains garbage, which is a fatal error!
+	MESSAGE(FATAL_ERROR "The file ${MUHKUH_VERSION_FILE} contains no valid version in the form number.number.hash .")
+ENDIF("${MUHKUH_VERSION_ALL}" STREQUAL "")
+
+STRING(REGEX MATCH ${MUHKUH_VERSION_REGEX} MUHKUH_VERSION_ALL ${MUHKUH_VERSION_ALL})
+SET(MUHKUH_VERSION_MAJ ${CMAKE_MATCH_1})
+SET(MUHKUH_VERSION_MIN ${CMAKE_MATCH_2})
+SET(MUHKUH_VERSION_SUB ${CMAKE_MATCH_3})
+
+
+
+# Get the VCS version.
+IF(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
+	FIND_PROGRAM(HG_EXECUTABLE
+	             NAMES hg
+	             PATH_SUFFIXES Mercurial
+	             DOC "hg command line client"
+	)
+	
+	IF(HG_EXECUTABLE)
+		EXECUTE_PROCESS(COMMAND ${HG_EXECUTABLE} id -i ${CMAKE_CURRENT_SOURCE_DIR}
+		                OUTPUT_VARIABLE MUHKUH_VERSION_VCS
+		                ERROR_QUIET
+		                OUTPUT_STRIP_TRAILING_WHITESPACE)
+	ELSE(HG_EXECUTABLE)
+		MESSAGE(WARNING "No Mercurial client found, can not get id of the working folder.")
+		SET(MUHKUH_VERSION_VCS "unknown")
+	ENDIF(HG_EXECUTABLE)
+ELSEIF(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg_archival.txt")
+	FILE(STRINGS "${CMAKE_HOME_DIRECTORY}/.hg_archival.txt" TMP_NODE_LINE REGEX ${HG_ARCHIVAL_VERSION_REGEX})
+	IF("${TMP_NODE_LINE}" STREQUAL "")
 		# No valid version number found. The file contains garbage, which is a fatal error!
-		MESSAGE(FATAL_ERROR "The file ${MUHKUH_VERSION_FILE} contains no valid version in the form number.number.hash .")
-	ELSE("${MUHKUH_VERSION_ALL}" STREQUAL "")
-		STRING(REGEX MATCH "${MUHKUH_VERSION_REXEX}" MUHKUH_VERSION_ALL ${MUHKUH_VERSION_ALL})
-		SET(MUHKUH_VERSION_MAJ ${CMAKE_MATCH_1})
-		SET(MUHKUH_VERSION_MIN ${CMAKE_MATCH_2})
-		SET(MUHKUH_VERSION_SUB ${CMAKE_MATCH_3})
-		MESSAGE("File ${MUHKUH_VERSION_FILE} found, version info: ${MUHKUH_VERSION_MAJ}.${MUHKUH_VERSION_MIN}.${MUHKUH_VERSION_SUB}")
-	ENDIF("${MUHKUH_VERSION_ALL}" STREQUAL "")
-ELSE(EXISTS ${MUHKUH_VERSION_FILE})
-	SET(MUHKUH_VERSION_MAJ 1)
-	SET(MUHKUH_VERSION_MIN 0)
-	SET(MUHKUH_VERSION_SUB 0)
+		MESSAGE(FATAL_ERROR "The file ${CMAKE_HOME_DIRECTORY}/.hg_archival.txt has an unknown format!")
+	ENDIF("${TMP_NODE_LINE}" STREQUAL "")
+	
+	STRING(REGEX MATCH ${HG_ARCHIVAL_VERSION_REGEX} TMP_NODE_LINE ${TMP_NODE_LINE})
+	SET(MUHKUH_VERSION_VCS ${CMAKE_MATCH_1})
+ELSE(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
+	MESSAGE(WARNING "The code is no mercurial repository and no mercurial archive.")
+	SET(MUHKUH_VERSION_VCS "unknown")
+ENDIF(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
 
-	IF(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
-		FIND_PROGRAM(HG_EXECUTABLE
-		             NAMES hg
-		             PATH_SUFFIXES Mercurial
-		             DOC "hg command line client"
-		)
 
-		IF(HG_EXECUTABLE)
-			MESSAGE("Mercurial data and client found, generating version info from revision number.")
-			EXECUTE_PROCESS(COMMAND ${HG_EXECUTABLE} id -i ${CMAKE_CURRENT_SOURCE_DIR}
-			                OUTPUT_VARIABLE MUHKUH_VERSION_SUB
-			                ERROR_QUIET
-			                OUTPUT_STRIP_TRAILING_WHITESPACE)
-			MESSAGE("Current revision is ${MUHKUH_VERSION_SUB}")
-		ELSE(HG_EXECUTABLE)
-			MESSAGE(WARNING "No Mercurial client found, using a fixed version.")
-		ENDIF(HG_EXECUTABLE)
-	ELSE(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
-		MESSAGE(WARNING "No file ${MUHKUH_VERSION_FILE} and no mercurial data found, using a fixed version.")
-	ENDIF(EXISTS "${CMAKE_HOME_DIRECTORY}/.hg")
-ENDIF(EXISTS ${MUHKUH_VERSION_FILE})
+MESSAGE("Version info: ${MUHKUH_VERSION_MAJ}.${MUHKUH_VERSION_MIN}.${MUHKUH_VERSION_SUB}, VCS ID is ${MUHKUH_VERSION_VCS} .")
+
 
 # Get the current year.
 # There seems to be no built-in way in cmake to get the current year.
