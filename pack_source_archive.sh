0e8c3a3c03ad27d231ca061d2c3496455c1f4bdd
doc_bacardi <devnull@localhost>
2013-10-21 15:03:52 +0200

    Add first try of a script to compress all sources and the firmware.

---
+++ pack_source_archive.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# The source archive conatins the firmware patch files. Make sure that they are up to date.
+python mbs/mbs
+
+# Get the HG id. Note: Do not use the placeholders of the archive command, or the archive filename is unknown for this script.
+HG_ID=`hg id -i`
+
+# Get the main muhkuh version numbers from the muhkuh_version.txt file.
+MUHKUH_VERSION_MAJ=`awk -F \. {'print $1'} muhkuh_version.txt`
+MUHKUH_VERSION_MIN=`awk -F \. {'print $2'} muhkuh_version.txt`
+MUHKUH_VERSION_SUB=`awk -F \. {'print $3'} muhkuh_version.txt`
+
+# Get the prefix. It is used as the prefix for the archive name and the first folder inside the archive.
+MUHKUH_PREFIX=muhkuh-${MUHKUH_VERSION_MAJ}.${MUHKUH_VERSION_MIN}.${MUHKUH_VERSION_SUB}-${HG_ID}
+
+# Create the archive. Use the uncompressed tar format. Compressed formats can not be updated.
+hg archive --prefix ${MUHKUH_PREFIX} --type tar --subrepos ${MUHKUH_PREFIX}.tar
+
+# Add the firmware patch files to the archive.
+tar --append --gzip --file ${MUHKUH_PREFIX}.tar plugins/romloader/uart/netx/targets/uartmon_firmware.diff plugins/romloader/usb/netx/targets/usbmon_firmware.diff
