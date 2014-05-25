256982050d1c996b58b0bcee0f7468b1ec43e403
doc_bacardi <devnull@localhost>
2014-05-25 16:44:46 +0200

    Start CPU detection.

--- plugins/romloader/jtag/openocd/romloader_jtag_openocd.c
+++ plugins/romloader/jtag/openocd/romloader_jtag_openocd.c
@@ -289,70 +289,224 @@ int romloader_jtag_openocd_init(ROMLOADER_JTAG_DEVICE_T *ptJtagDevice)
 }
 
 
-static const char *apcIfCfg[2] =
+typedef struct INIT_CODE_WITH_ID_STRUCT
 {
-  "# This interface allows only one transport: jtag.\n"
-  "interface ftdi\n"
-  "ftdi_device_desc \"Amontec JTAGkey\"\n"
-  "ftdi_vid_pid 0x0403 0xcff8\n"
-  "# Use a ridiculously low speed to ensure that the connection is working in absolutely all circumstances (even with Enricos flying wires ;) ).\n"
-  "adapter_khz 100\n"
-  "\n"
-  "ftdi_layout_init 0x0c08 0x0f1b\n"
-  "ftdi_layout_signal nTRST -data 0x0100 -noe 0x0400\n"
-  "ftdi_layout_signal nSRST -data 0x0200 -noe 0x0800\n"
-  "set IF_CFG_RESULT [ catch {jtag init} ]\n",
+	const char *pcID;
+	const char *pcCode;
+} INIT_CODE_WITH_ID_T;
 
-  "interface ftdi\n"
-  "ftdi_device_desc \"NXHX50-RE\"\n"
-  "ftdi_vid_pid 0x0640 0x0028\n"
-  "# Use a ridiculously low speed to ensure that the connection is working in absolutely all circumstances (even with Enricos flying wires ;) ).\n"
-  "adapter_khz 100\n"
-  "\n"
-  "ftdi_layout_init 0x0308 0x030b\n"
-  "ftdi_layout_signal nTRST -data 0x0100\n"
-  "ftdi_layout_signal nSRST -data 0x0200\n"
-  "set IF_CFG_RESULT [ catch {jtag init} ]\n"
+
+/* This array contains the detection command sequence for all interfaces. All
+ * commands for one interface are stored in one string. The commands are
+ * separated by newlines.
+ *
+ * Every detection sequence must set the TCL variable 'IF_CFG_RESULT'. It
+ * shows the result of the detection. A value of '1' 
+ */
+static const INIT_CODE_WITH_ID_T atIfCfg[2] =
+{
+	{
+		.pcID = "Amontec JTAGkey",
+		.pcCode = "set RESULT 0\n"
+		          "\n"
+		          "# This interface allows only one transport: jtag.\n"
+		          "interface ftdi\n"
+		          "ftdi_device_desc \"Amontec JTAGkey\"\n"
+		          "ftdi_vid_pid 0x0403 0xcff8\n"
+		          "# Use a ridiculously low speed to ensure that the connection is working in absolutely all circumstances (even with Enricos flying wires ;) ).\n"
+		          "adapter_khz 100\n"
+		          "\n"
+		          "ftdi_layout_init 0x0c08 0x0f1b\n"
+		          "ftdi_layout_signal nTRST -data 0x0100 -noe 0x0400\n"
+		          "ftdi_layout_signal nSRST -data 0x0200 -noe 0x0800\n"
+		          "if {[ catch {jtag init} ]==0 } {\n"
+		          "    set RESULT {OK}\n"
+		          "}\n"
+		          "return $RESULT\n"
+	},
+
+	{
+		.pcID = "NXHX50-RE",
+		.pcCode = "set RESULT 0\n"
+		          "\n"
+		          "interface ftdi\n"
+		          "ftdi_device_desc \"NXHX50-RE\"\n"
+		          "ftdi_vid_pid 0x0640 0x0028\n"
+		          "# Use a ridiculously low speed to ensure that the connection is working in absolutely all circumstances (even with Enricos flying wires ;) ).\n"
+		          "adapter_khz 100\n"
+		          "\n"
+		          "ftdi_layout_init 0x0308 0x030b\n"
+		          "ftdi_layout_signal nTRST -data 0x0100\n"
+		          "ftdi_layout_signal nSRST -data 0x0200\n"
+		          "if {[ catch {jtag init} ]==0 } {\n"
+		          "    set RESULT {OK}\n"
+		          "}\n"
+		          "return $RESULT\n"
+	}
 };
 
-static const char *pcIfCleanup = "interface_clear\ntransport clear\n";
+
+static const char *pcIfCleanup = "interface_clear\ntransport clear\njtag cleartaps\n";
+
+
+static const INIT_CODE_WITH_ID_T atScCfg[2] =
+{
+	{
+		.pcID = "netX_ARM966",
+		.pcCode = "global SC_CFG_RESULT\n"
+		          "\n"
+		          "echo {Detecting netX_ARM966 scan chain}\n"
+		          "\n"
+		          "jtag cleartaps\n"
+		          "\n"
+		          "set SC_CFG_RESULT 0\n"
+		          "\n"
+		          "jtag newtap netX_ARM966 cpu -irlen 4 -ircapture 1 -irmask 0xf -expected-id 0x25966021\n"
+		          "jtag configure netX_ARM966.cpu -event setup { global SC_CFG_RESULT ; echo {setup done!} ; set SC_CFG_RESULT {OK} }\n"
+		          "jtag arp_init\n"
+		          "\n"
+		          "if { $SC_CFG_RESULT=={OK} } {\n"
+		          "    echo {init target}\n"
+		          "    target create netX_ARM966.cpu arm966e -endian little -chain-position netX_ARM966.cpu\n"
+		          "}\n"
+		          "\n"
+		          "return $SC_CFG_RESULT\n"
+	},
+
+	{
+		.pcID = "netX_ARM926",
+		.pcCode = "global SC_CFG_RESULT\n"
+		          "\n"
+		          "echo {Detecting netX_ARM926 scan chain}\n"
+		          "\n"
+		          "jtag cleartaps\n"
+		          "\n"
+		          "set SC_CFG_RESULT 0\n"
+		          "\n"
+		          "jtag newtap netX_ARM926 cpu -irlen 4 -ircapture 1 -irmask 0xf -expected-id 0x07926021\n"
+		          "jtag configure netX_ARM926.cpu -event setup { global SC_CFG_RESULT ; echo {setup done!} ; set SC_CFG_RESULT {OK} }\n"
+		          "jtag arp_init\n"
+		          "\n"
+		          "if { $SC_CFG_RESULT=={OK} } {\n"
+		          "    echo {init target}\n"
+		          "    target create netX_ARM926.cpu arm926ejs -endian little -chain-position netX_ARM926.cpu\n"
+		          "}\n"
+		          "\n"
+		          "return $SC_CFG_RESULT\n"
+	}
+};
+
 
 
 int get_result(struct command_context *ptCmdCtx, char *pcBuffer, size_t sizBufferMax)
 {
-  Jim_Interp *ptInterp;
-  Jim_Obj *ptResultObj;
-  const char *pcResult;
-  int iResLen;
-  int iResult;
+	Jim_Interp *ptInterp;
+	Jim_Obj *ptResultObj;
+	const char *pcResult;
+	int iResLen;
+	int iResult;
+	
+	
+	/* Be pessimistic. */
+	iResult = -1;
+	
+	ptInterp = ptCmdCtx->interp;
+	
+	ptResultObj = Jim_GetResult(ptInterp);
+	if( ptResultObj!=NULL )
+	{
+		iResLen = 0;
+		pcResult = Jim_GetString(ptResultObj, &iResLen);
+		fprintf(stderr, "Jim_GetString: %p %d\n", pcResult, iResLen);
+		
+		/* Does the result still exist? */
+		if( pcResult!=NULL && iResLen>0 )
+		{
+			/* The size of the result must be smaller and not equal because it must be terminated with a 0 byte. */
+			if( iResLen<sizBufferMax )
+			{
+				strncpy(pcBuffer, pcResult, iResLen);
+				pcBuffer[iResLen] = '\0';
+				iResult = 0;
+			}
+		}
+	}
+	
+	return iResult;
+}
 
 
-  /* Be pessimistic. */
-  iResult = -1;
 
-  ptInterp = ptCmdCtx->interp;
+static int romloader_jtag_openocd_detect_scan_chain(struct command_context *ptCmdCtx)
+{
+	const INIT_CODE_WITH_ID_T *ptCnt;
+	const INIT_CODE_WITH_ID_T *ptEnd;
+	int iResult;
+	int sizResult;
+	char strResult[256];
 
-  ptResultObj = Jim_GetResult(ptInterp);
-  if( ptResultObj!=NULL )
-  {
-    iResLen = 0;
-    pcResult = Jim_GetString(ptResultObj, &iResLen);
-    fprintf(stderr, "Jim_GetString: %p %d\n", pcResult, iResLen);
 
-    /* Does the result still exist? */
-    if( pcResult!=NULL && iResLen>0 )
-    {
-      /* The size of the result must be smaller and not equal because it must be terminated with a 0 byte. */
-      if( iResLen<sizBufferMax )
-      {
-	strncpy(pcBuffer, pcResult, iResLen);
-	pcBuffer[iResLen] = '\0';
-	iResult = 0;
-      }
-    }
-  }
+	/* Try to run all command chunks to see which CPU is there. */
+	ptCnt = atScCfg;
+	ptEnd = atScCfg + (sizeof(atScCfg)/sizeof(atScCfg[0]));
+	while( ptCnt<ptEnd )
+	{
+		fprintf(stderr, "Detecting CPU %s\n", ptCnt->pcID);
+		iResult = command_run_line(ptCmdCtx, ptCnt->pcCode);
+		fprintf(stderr, "iResult: %d\n", iResult);
+		
+		iResult = get_result(ptCmdCtx, strResult, sizeof(strResult));
+		fprintf(stderr, "iResult: %d\n", iResult);
+		fprintf(stderr, "RESULT: '%s'\n", strResult);
+		if( strncmp(strResult, "OK", 3)==0 )
+		{
+			fprintf(stderr, "Found scan chain!\n");
+			break;
+		}
+		
+		/* Move to the next configuration. */
+		++ptCnt;
+	}
+}
 
-  return iResult;
+
+
+static int romloader_jtag_openocd_detect_interface(struct command_context *ptCmdCtx)
+{
+	const INIT_CODE_WITH_ID_T *ptCnt;
+	const INIT_CODE_WITH_ID_T *ptEnd;
+	int iResult;
+	int sizResult;
+	char strResult[256];
+
+
+	/* Try to run all command chunks to see which interfaces are present. */
+	ptCnt = atIfCfg;
+	ptEnd = atIfCfg + (sizeof(atIfCfg)/sizeof(atIfCfg[0]));
+	while( ptCnt<ptEnd )
+	{
+		fprintf(stderr, "Detecting IF %s\n", ptCnt->pcID);
+		iResult = command_run_line(ptCmdCtx, ptCnt->pcCode);
+		fprintf(stderr, "iResult: %d\n", iResult);
+		
+		iResult = get_result(ptCmdCtx, strResult, sizeof(strResult));
+		fprintf(stderr, "iResult: %d\n", iResult);
+		fprintf(stderr, "RESULT: '%s'\n", strResult);
+		if( strncmp(strResult, "OK", 3)==0 )
+		{
+			fprintf(stderr, "Found Interface!\n");
+			
+			/* Now detect the CPU. */
+			romloader_jtag_openocd_detect_scan_chain(ptCmdCtx);
+		}
+		
+		/* Detection finished, clean up. */
+		iResult = command_run_line(ptCmdCtx, pcIfCleanup);
+		fprintf(stderr, "If Cleanup Result: %d\n", iResult);
+		
+		/* Move to the next configuration. */
+		++ptCnt;
+	}
 }
 
 
@@ -360,40 +514,12 @@ int get_result(struct command_context *ptCmdCtx, char *pcBuffer, size_t sizBuffe
 int romloader_jtag_openocd_detect(ROMLOADER_JTAG_DEVICE_T *ptJtagDevice)
 {
 	struct command_context *ptCmdCtx;
-	const char **ppcIfCfgCnt;
-	const char **ppcIfCfgEnd;
-	int iResult;
-	int sizResult;
-	char strResult[256];
-	int iTclResult;
 
 
 	ptCmdCtx = (struct command_context *)(ptJtagDevice->pvOpenocdContext);
 	if( ptCmdCtx!=NULL )
 	{
-		/* Try to run all command chunks to see which interfaces are present. */
-		ppcIfCfgCnt = apcIfCfg;
-		ppcIfCfgEnd = apcIfCfg + (sizeof(apcIfCfg)/sizeof(apcIfCfg[0]));
-		while( ppcIfCfgCnt<ppcIfCfgEnd )
-		{
-			fprintf(stderr, "Running command ***%s***\n", *ppcIfCfgCnt);
-			iResult = command_run_line(ptCmdCtx, *ppcIfCfgCnt);
-			fprintf(stderr, "If Cfg Result: %d\n", iResult);
-			
-			iResult = get_result(ptCmdCtx, strResult, sizeof(strResult));
-			fprintf(stderr, "IF_CFG_RESULT: '%s'\n", strResult);
-			iResult = sscanf(strResult, "%d", &iTclResult);
-			if( iResult==1 && iTclResult==0 )
-			{
-			}
-			
-			/* Detection finished, clean up. */
-			iResult = command_run_line(ptCmdCtx, pcIfCleanup);
-			fprintf(stderr, "If Cleanup Result: %d\n", iResult);
-			
-			/* Move to the next configuration. */
-			++ppcIfCfgCnt;
-		}
+		romloader_jtag_openocd_detect_interface(ptCmdCtx);
 	}
 	
 	return 0;
