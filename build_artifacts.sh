e8a88104bc4bdb70636d4da7aded54c2694a52aa
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2014-09-02 18:10:56 +0200

    Add build script.

---
+++ build_artifacts.sh
@@ -0,0 +1,80 @@
+#! /bin/sh
+# sudo apt-get --quiet --assume-yes install mingw-w64 g++-mingw-w64 gcc-multilib g++-multilib make cmake swig zip libudev-dev libreadline6-dev
+
+
+#-----------------------------------------------------------------------------
+#
+# Build the netX firmware.
+#
+python mbs/mbs
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+
+#-----------------------------------------------------------------------------
+#
+# Build the windows32 version.
+#
+rm -rf build_windows32
+mkdir build_windows32
+cd build_windows32
+
+cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=x86 -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE ..
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make install DESTDIR=`pwd`/install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+cd ..
+
+
+#-----------------------------------------------------------------------------
+#
+# Build the windows64 version.
+#
+rm -rf build_windows64
+mkdir build_windows64
+cd build_windows64
+
+cmake -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=amd64 -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE ..
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make install DESTDIR=`pwd`/install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+cd ..
+
+
+#-----------------------------------------------------------------------------
+#
+# Assemble the artifacts.
+#
+rm -rf build
+mkdir build
+cd build
+cmake ../installer/ivy
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
