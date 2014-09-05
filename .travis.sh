3eca97f88c63e4dd6cb8ef2b2294281f2cdfd73f
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2014-09-05 10:52:06 +0200

    Use new swig3 package with "swig" executable.

--- .travis.sh
+++ .travis.sh
@@ -19,7 +19,7 @@ rm -rf build_windows32
 mkdir build_windows32
 cd build_windows32
 
-cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=x86 -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE -DSWIG_EXECUTABLE=/usr/bin/swig3.0 ..
+cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=x86 -DCMAKE_C_COMPILER=/usr/bin/i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/i686-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE ..
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
 	exit 1
@@ -42,7 +42,7 @@ rm -rf build_windows64
 mkdir build_windows64
 cd build_windows64
 
-cmake -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=amd64 -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE -DSWIG_EXECUTABLE=/usr/bin/swig3.0 ..
+cmake -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=amd64 -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE ..
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
 	exit 1
