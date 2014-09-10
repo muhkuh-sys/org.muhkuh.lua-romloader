368fe75508e09a77d2f959902abb312db754256a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2014-09-10 17:50:50 +0200

    Run tests.

--- .travis.sh
+++ .travis.sh
@@ -25,6 +25,18 @@ if [ $STATUS -ne 0 ]; then
 	exit 1
 fi
 
+make
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make test
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
 make install DESTDIR=`pwd`/install
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
@@ -43,6 +55,18 @@ mkdir build_windows64
 cd build_windows64
 
 cmake -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=amd64 -DCMAKE_C_COMPILER=/usr/bin/x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=/usr/bin/x86_64-w64-mingw32-windres -DUSE_SYSTEM_LUA=FALSE -DSWIG_EXECUTABLE=/usr/bin/swig3.0 ..
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make test
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
 	exit 1
