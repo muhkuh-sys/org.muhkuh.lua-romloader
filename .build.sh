efffa87a7bc2a0a6ce45d9a1240aba12fb9c42f1
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-06-29 14:20:43 +0200

    Stop on first error instead of repeated status checks.

--- .build.sh
+++ .build.sh
@@ -1,14 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
 #-----------------------------------------------------------------------------
 #
 # Build the netX firmware.
 #
 python mbs/mbs
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
 
 
 #-----------------------------------------------------------------------------
@@ -17,33 +13,12 @@ fi
 #
 rm -rf build_windows32
 mkdir build_windows32
-cd build_windows32
-
+pushd build_windows32
 cmake -DCMAKE_INSTALL_PREFIX="" -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=x86 -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres -DLUA_LIBRARIES=${LUA_LIBRARIES_X86} -DLUA_INCLUDE_DIR=${LUA_INCLUDE_DIR_X86} ..
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make test
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make install DESTDIR=`pwd`/install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-cd ..
+popd
 
 
 #-----------------------------------------------------------------------------
@@ -52,33 +27,12 @@ cd ..
 #
 rm -rf build_windows64
 mkdir build_windows64
-cd build_windows64
-
+pushd build_windows64
 cmake -DCMAKE_INSTALL_PREFIX="" -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DTARGET_PROCESSOR=amd64 -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres -DLUA_LIBRARIES=${LUA_LIBRARIES_X86_64} -DLUA_INCLUDE_DIR=${LUA_INCLUDE_DIR_X86_64} ..
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make test
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make install DESTDIR=`pwd`/install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-cd ..
+popd
 
 
 #-----------------------------------------------------------------------------
@@ -87,18 +41,8 @@ cd ..
 #
 rm -rf build
 mkdir build
-cd build
+pushd build
 cmake ../installer/ivy
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
 make
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-cd ..
+popd
 
