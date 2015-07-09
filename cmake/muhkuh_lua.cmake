0d3e44e488480be44f596cbd9505a381e4c8bb3d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-07-09 21:30:59 +0200

    Use system LUA on linux.

--- cmake/muhkuh_lua.cmake
+++ cmake/muhkuh_lua.cmake
@@ -4,5 +4,14 @@
 # to choose which lua version should be used.
 #
 
-# Use the system version.
-FIND_PACKAGE(org.muhkuh.lua-lua51 REQUIRED)
+# Use the MBS LUA version on Windows.
+IF((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+	FIND_PACKAGE(org.muhkuh.lua-lua51 REQUIRED)
+	SET(LUA51_FOUND 1)
+	GET_TARGET_PROPERTY(LUA_LIBRARIES org.muhkuh.lua-lua51::TARGET_lualib LOCATION)
+	GET_TARGET_PROPERTY(LUA_INCLUDE_DIR org.muhkuh.lua-lua51::TARGET_lualib INTERFACE_INCLUDE_DIRECTORIES)
+	SET(LUA_VERSION_STRING ${org.muhkuh.lua-lua51_VERSION})
+ELSE((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+	FIND_PACKAGE(Lua51 REQUIRED)
+ENDIF((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+
