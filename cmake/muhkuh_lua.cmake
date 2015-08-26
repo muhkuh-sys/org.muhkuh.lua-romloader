ee9fcd78e8bffe27733db52d30791fb5fcebecaf
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-08-26 18:02:14 +0200

    Get the paths to the build dependencies from IVY.

--- cmake/muhkuh_lua.cmake
+++ cmake/muhkuh_lua.cmake
@@ -6,6 +6,13 @@
 
 # Use the MBS LUA version on Windows.
 IF((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+	# Get the path to LUA from the ant system.
+	IF(CMAKE_SIZEOF_VOID_P MATCHES 8)
+		SET(org.muhkuh.lua-lua51_DIR ${ant_path.org.muhkuh.lua.lua51}/windows_amd64/cmake)
+	ELSE(CMAKE_SIZEOF_VOID_P MATCHES 8)
+		SET(org.muhkuh.lua-lua51_DIR ${ant_path.org.muhkuh.lua.lua51}/windows_x86/cmake)
+	ENDIF(CMAKE_SIZEOF_VOID_P MATCHES 8)
+	
 	FIND_PACKAGE(org.muhkuh.lua-lua51 REQUIRED)
 	SET(LUA51_FOUND 1)
 	GET_TARGET_PROPERTY(LUA_LIBRARIES org.muhkuh.lua-lua51::TARGET_lualib LOCATION)
