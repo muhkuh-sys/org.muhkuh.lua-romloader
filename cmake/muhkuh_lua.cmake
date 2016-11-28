3f4a2fafeea48d104477069cd42b16b5f541da47
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-11-28 17:58:22 +0100

    Build the external projects as CMAKE components. Build LUA5.2 and LUA5.3 versions.

--- cmake/muhkuh_lua.cmake
+++ cmake/muhkuh_lua.cmake
@@ -4,20 +4,41 @@
 # to choose which lua version should be used.
 #
 
+OPTION(BUILDCFG_LUA_VERSION "Select the LUA version used in this build. Possible values are 5.1, 5.2 and 5.3. The default is 5.1."
+       "5.1")
+
 # Use the MBS LUA version on Windows.
 IF((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
 	# Get the path to LUA from the ant system.
 	IF(CMAKE_SIZEOF_VOID_P MATCHES 8)
-		SET(org.muhkuh.lua-lua51_DIR ${ant_path.org.muhkuh.lua.lua51}/windows_amd64/cmake)
+		LIST(APPEND CMAKE_PREFIX_PATH ${CMAKE_HOME_DIRECTORY}/jonchki/build_requirements/windows_x64/cmake)
 	ELSE(CMAKE_SIZEOF_VOID_P MATCHES 8)
-		SET(org.muhkuh.lua-lua51_DIR ${ant_path.org.muhkuh.lua.lua51}/windows_x86/cmake)
+		LIST(APPEND CMAKE_PREFIX_PATH ${CMAKE_HOME_DIRECTORY}/jonchki/build_requirements/windows_x86/cmake)
 	ENDIF(CMAKE_SIZEOF_VOID_P MATCHES 8)
-	
-	FIND_PACKAGE(org.muhkuh.lua-lua51 REQUIRED)
-	SET(LUA_FOUND 1)
-	GET_TARGET_PROPERTY(LUA_LIBRARIES org.muhkuh.lua-lua51::TARGET_lualib LOCATION)
-	GET_TARGET_PROPERTY(LUA_INCLUDE_DIR org.muhkuh.lua-lua51::TARGET_lualib INTERFACE_INCLUDE_DIRECTORIES)
-	SET(LUA_VERSION_STRING ${org.muhkuh.lua-lua51_VERSION})
+
+	IF((${BUILDCFG_LUA_VERSION} STREQUAL "5.1"))
+		FIND_PACKAGE(org.lua.lua-lua51 REQUIRED)
+		SET(LUA_FOUND 1)
+		GET_TARGET_PROPERTY(LUA_LIBRARIES org.lua.lua-lua51::TARGET_lualib LOCATION)
+		GET_TARGET_PROPERTY(LUA_INCLUDE_DIR org.lua.lua-lua51::TARGET_lualib INTERFACE_INCLUDE_DIRECTORIES)
+		SET(LUA_VERSION_STRING ${org.lua.lua-lua51_VERSION})
+	ELSEIF((${BUILDCFG_LUA_VERSION} STREQUAL "5.2"))
+		FIND_PACKAGE(org.lua.lua-lua52 REQUIRED)
+		SET(LUA_FOUND 1)
+		GET_TARGET_PROPERTY(LUA_LIBRARIES org.lua.lua-lua52::TARGET_lualib LOCATION)
+		GET_TARGET_PROPERTY(LUA_INCLUDE_DIR org.lua.lua-lua52::TARGET_lualib INTERFACE_INCLUDE_DIRECTORIES)
+		SET(LUA_VERSION_STRING ${org.lua.lua-lua52_VERSION})
+	ELSEIF((${BUILDCFG_LUA_VERSION} STREQUAL "5.3"))
+		FIND_PACKAGE(org.lua.lua-lua53 REQUIRED)
+		SET(LUA_FOUND 1)
+		GET_TARGET_PROPERTY(LUA_LIBRARIES org.lua.lua-lua53::TARGET_lualib LOCATION)
+		GET_TARGET_PROPERTY(LUA_INCLUDE_DIR org.lua.lua-lua53::TARGET_lualib INTERFACE_INCLUDE_DIRECTORIES)
+		SET(LUA_VERSION_STRING ${org.lua.lua-lua53_VERSION})
+	ELSE((${BUILDCFG_LUA_VERSION} STREQUAL "5.1"))
+		# Stop here!
+		MESSAGE(FATAL_ERROR "The value '${BUILDCFG_LUA_VERSION}' for BUILDCFG_LUA_VERSION is invalid!")
+	ENDIF((${BUILDCFG_LUA_VERSION} STREQUAL "5.1"))
 ELSE((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
 	FIND_PACKAGE(Lua 5.1 REQUIRED EXACT)
 ENDIF((${CMAKE_SYSTEM_NAME} STREQUAL "Windows"))
+
