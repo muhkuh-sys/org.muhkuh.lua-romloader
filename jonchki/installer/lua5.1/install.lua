160a7e10372b120ac6b10ce76024c0f53da33c64
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-09 13:40:20 +0100

    Rename Windows_x86 to windows_32 and Windows_x64 to windows_64.

--- jonchki/installer/lua5.1/install.lua
+++ jonchki/installer/lua5.1/install.lua
@@ -1,22 +1,22 @@
 local t = ...
+local strPlatform = t:get_platform()
 
-local strPlatform = 'Windows_x64'
 
-if strPlatform=='Windows_x86' then
-	t:install('windows_x86/lua_plugins/romloader.dll', '${install_lua_cpath}')
-	t:install('windows_x86/lua_plugins/romloader_eth.dll', '${install_lua_cpath}')
-	t:install('windows_x86/lua_plugins/romloader_jtag.dll', '${install_lua_cpath}')
-	t:install('windows_x86/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}')
-	t:install('windows_x86/lua_plugins/romloader_uart.dll', '${install_lua_cpath}')
-	t:install('windows_x86/lua_plugins/romloader_usb.dll', '${install_lua_cpath}')
+if strPlatform=='windows_32' then
+	t:install('windows_32/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
+	t:install('windows_32/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
+	t:install('windows_32/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
+	t:install('windows_32/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
+	t:install('windows_32/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
+	t:install('windows_32/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
 
-elseif strPlatform=='Windows_x64' then
-	t:install('windows_x64/lua_plugins/romloader.dll', '${install_lua_cpath}')
-	t:install('windows_x64/lua_plugins/romloader_eth.dll', '${install_lua_cpath}')
-	t:install('windows_x64/lua_plugins/romloader_jtag.dll', '${install_lua_cpath}')
-	t:install('windows_x64/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}')
-	t:install('windows_x64/lua_plugins/romloader_uart.dll', '${install_lua_cpath}')
-	t:install('windows_x64/lua_plugins/romloader_usb.dll', '${install_lua_cpath}')
+elseif strPlatform=='windows_64' then
+	t:install('windows_64/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
+	t:install('windows_64/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
+	t:install('windows_64/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
+	t:install('windows_64/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
+	t:install('windows_64/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
+	t:install('windows_64/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
 
 end
 
