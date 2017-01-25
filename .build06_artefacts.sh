65aec2f4a995049b8398f518f83894ea9e205339
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-01-25 14:19:08 +0100

    Build the artifacts.

--- .build06_artefacts.sh
+++ .build06_artefacts.sh
@@ -5,24 +5,105 @@ set -e
 #
 # Build all artefacts.
 #
-rm -rf build
-mkdir build
+pushd build
+rm -rf org.muhkuh.lua-lua5.1-romloader
+rm -rf org.muhkuh.lua-lua5.2-romloader
+rm -rf org.muhkuh.lua-lua5.3-romloader
+rm -rf lua5.1
+rm -rf lua5.2
+rm -rf lua5.3
 
-mkdir build/org.muhkuh.lua-lua5.1-romloader
+mkdir org.muhkuh.lua-lua5.1-romloader
+mkdir org.muhkuh.lua-lua5.2-romloader
+mkdir org.muhkuh.lua-lua5.3-romloader
+mkdir -p lua5.1/windows_x86
+mkdir -p lua5.1/windows_x86_64
+mkdir -p lua5.1/ubuntu_1204_x86
+mkdir -p lua5.1/ubuntu_1204_x86_64
+mkdir -p lua5.1/ubuntu_1404_x86
+mkdir -p lua5.1/ubuntu_1404_x86_64
+mkdir -p lua5.1/ubuntu_1604_x86
+mkdir -p lua5.1/ubuntu_1604_x86_64
+mkdir -p lua5.1/ubuntu_1610_x86
+mkdir -p lua5.1/ubuntu_1610_x86_64
+mkdir -p lua5.2/windows_x86
+mkdir -p lua5.2/windows_x86_64
+mkdir -p lua5.2/ubuntu_1204_x86
+mkdir -p lua5.2/ubuntu_1204_x86_64
+mkdir -p lua5.2/ubuntu_1404_x86
+mkdir -p lua5.2/ubuntu_1404_x86_64
+mkdir -p lua5.2/ubuntu_1604_x86
+mkdir -p lua5.2/ubuntu_1604_x86_64
+mkdir -p lua5.2/ubuntu_1610_x86
+mkdir -p lua5.2/ubuntu_1610_x86_64
+mkdir -p lua5.3/windows_x86
+mkdir -p lua5.3/windows_x86_64
+mkdir -p lua5.3/ubuntu_1204_x86
+mkdir -p lua5.3/ubuntu_1204_x86_64
+mkdir -p lua5.3/ubuntu_1404_x86
+mkdir -p lua5.3/ubuntu_1404_x86_64
+mkdir -p lua5.3/ubuntu_1604_x86
+mkdir -p lua5.3/ubuntu_1604_x86_64
+mkdir -p lua5.3/ubuntu_1610_x86
+mkdir -p lua5.3/ubuntu_1610_x86_64
+
+
+
+tar --extract --directory lua5.1/windows_x86 --file build_lua5.1_windows_x86.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86 --file build_lua5.2_windows_x86.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86 --file build_lua5.3_windows_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/windows_x86_64 --file build_lua5.1_windows_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86_64 --file build_lua5.2_windows_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86_64 --file build_lua5.3_windows_x86_64.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1204_x86 --file build_lua5.1_ubuntu_1204_x86.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1204_x86 --file build_lua5.2_ubuntu_1204_x86.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1204_x86 --file build_lua5.3_ubuntu_1204_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1204_x86_64 --file build_lua5.1_ubuntu_1204_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1204_x86_64 --file build_lua5.2_ubuntu_1204_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1204_x86_64 --file build_lua5.3_ubuntu_1204_x86_64.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86 --file build_lua5.1_ubuntu_1404_x86.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86 --file build_lua5.2_ubuntu_1404_x86.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86 --file build_lua5.3_ubuntu_1404_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86_64 --file build_lua5.1_ubuntu_1404_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86_64 --file build_lua5.2_ubuntu_1404_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86_64 --file build_lua5.3_ubuntu_1404_x86_64.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86 --file build_lua5.1_ubuntu_1604_x86.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86 --file build_lua5.2_ubuntu_1604_x86.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86 --file build_lua5.3_ubuntu_1604_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86_64 --file build_lua5.1_ubuntu_1604_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86_64 --file build_lua5.2_ubuntu_1604_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86_64 --file build_lua5.3_ubuntu_1604_x86_64.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1610_x86 --file build_lua5.1_ubuntu_1610_x86.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1610_x86 --file build_lua5.2_ubuntu_1610_x86.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1610_x86 --file build_lua5.3_ubuntu_1610_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1610_x86_64 --file build_lua5.1_ubuntu_1610_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1610_x86_64 --file build_lua5.2_ubuntu_1610_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1610_x86_64 --file build_lua5.3_ubuntu_1610_x86_64.tar.gz --gzip
+
+popd
+
+
 pushd build/org.muhkuh.lua-lua5.1-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.1
 make
 make package
 popd
 
-mkdir build/org.muhkuh.lua-lua5.2-romloader
 pushd build/org.muhkuh.lua-lua5.2-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.2
 make
 make package
 popd
 
-mkdir build/org.muhkuh.lua-lua5.3-romloader
 pushd build/org.muhkuh.lua-lua5.3-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.3
 make
--- jonchki/installer/lua5.2/install.lua
+++ jonchki/installer/lua5.2/install.lua
@@ -1,23 +1,92 @@
 local t = ...
-local strPlatform = t:get_platform()
+local strDistId, strDistVersion, strCpuArch = t:get_platform()
 
+if strDistId=='windows' then
+  if strCpuArch=='x86' then
+    t:install('windows_x86/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
+  elseif strCpuArch=='x86_64' then
+    t:install('windows_x86_64/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
+  end
 
-if strPlatform=='windows_32' then
-	t:install('windows_32/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
-	t:install('windows_32/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
-	t:install('windows_32/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
-	t:install('windows_32/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
-	t:install('windows_32/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
-	t:install('windows_32/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
+elseif strDistId=='ubuntu' then
+  if strDistVersion=='12.04' then
+    if strCpuArch=='x86' then
+      t:install('ubuntu_1204_x86/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1204_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    end
 
-elseif strPlatform=='windows_64' then
-	t:install('windows_64/lua_plugins/romloader.dll',               '${install_lua_cpath}/')
-	t:install('windows_64/lua_plugins/romloader_eth.dll',           '${install_lua_cpath}/')
-	t:install('windows_64/lua_plugins/romloader_jtag.dll',          '${install_lua_cpath}/')
-	t:install('windows_64/lua_plugins/romloader_papa_schlumpf.dll', '${install_lua_cpath}/')
-	t:install('windows_64/lua_plugins/romloader_uart.dll',          '${install_lua_cpath}/')
-	t:install('windows_64/lua_plugins/romloader_usb.dll',           '${install_lua_cpath}/')
+  if strDistVersion=='14.04' then
+    if strCpuArch=='x86' then
+      t:install('ubuntu_1404_x86/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    end
 
+    if strDistVersion=='16.04' then
+      if strCpuArch=='x86' then
+      t:install('ubuntu_1604_x86/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    end
+
+    if strDistVersion=='16.10' then
+      if strCpuArch=='x86' then
+      t:install('ubuntu_1610_x86/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
+      t:install('ubuntu_1610_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+    end
+  end
 end
 
 return true
