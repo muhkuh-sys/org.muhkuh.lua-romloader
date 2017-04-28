69c55964db82f1c615bd3a5e3b85009047b844df
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-04-28 11:53:52 +0200

    Support Ubuntu 17.04 . Drop support for Ubuntu 12.04 . Generate a POM file. Compress the artifact with tar.xz .

--- .build05_artefacts.sh
+++ .build05_artefacts.sh
@@ -18,34 +18,34 @@ mkdir org.muhkuh.lua-lua5.2-romloader
 mkdir org.muhkuh.lua-lua5.3-romloader
 mkdir -p lua5.1/windows_x86
 mkdir -p lua5.1/windows_x86_64
-mkdir -p lua5.1/ubuntu_1204_x86
-mkdir -p lua5.1/ubuntu_1204_x86_64
 mkdir -p lua5.1/ubuntu_1404_x86
 mkdir -p lua5.1/ubuntu_1404_x86_64
 mkdir -p lua5.1/ubuntu_1604_x86
 mkdir -p lua5.1/ubuntu_1604_x86_64
 mkdir -p lua5.1/ubuntu_1610_x86
 mkdir -p lua5.1/ubuntu_1610_x86_64
+mkdir -p lua5.1/ubuntu_1704_x86
+mkdir -p lua5.1/ubuntu_1704_x86_64
 mkdir -p lua5.2/windows_x86
 mkdir -p lua5.2/windows_x86_64
-mkdir -p lua5.2/ubuntu_1204_x86
-mkdir -p lua5.2/ubuntu_1204_x86_64
 mkdir -p lua5.2/ubuntu_1404_x86
 mkdir -p lua5.2/ubuntu_1404_x86_64
 mkdir -p lua5.2/ubuntu_1604_x86
 mkdir -p lua5.2/ubuntu_1604_x86_64
 mkdir -p lua5.2/ubuntu_1610_x86
 mkdir -p lua5.2/ubuntu_1610_x86_64
+mkdir -p lua5.2/ubuntu_1704_x86
+mkdir -p lua5.2/ubuntu_1704_x86_64
 mkdir -p lua5.3/windows_x86
 mkdir -p lua5.3/windows_x86_64
-mkdir -p lua5.3/ubuntu_1204_x86
-mkdir -p lua5.3/ubuntu_1204_x86_64
 mkdir -p lua5.3/ubuntu_1404_x86
 mkdir -p lua5.3/ubuntu_1404_x86_64
 mkdir -p lua5.3/ubuntu_1604_x86
 mkdir -p lua5.3/ubuntu_1604_x86_64
 mkdir -p lua5.3/ubuntu_1610_x86
 mkdir -p lua5.3/ubuntu_1610_x86_64
+mkdir -p lua5.3/ubuntu_1704_x86
+mkdir -p lua5.3/ubuntu_1704_x86_64
 
 
 
@@ -56,14 +56,6 @@ tar --extract --directory lua5.3/windows_x86 --file build_lua5.3_windows_x86.tar
 tar --extract --directory lua5.1/windows_x86_64 --file build_lua5.1_windows_x86_64.tar.gz --gzip
 tar --extract --directory lua5.2/windows_x86_64 --file build_lua5.2_windows_x86_64.tar.gz --gzip
 tar --extract --directory lua5.3/windows_x86_64 --file build_lua5.3_windows_x86_64.tar.gz --gzip
-
-tar --extract --directory lua5.1/ubuntu_1204_x86 --file build_lua5.1_ubuntu_1204_x86.tar.gz --gzip
-tar --extract --directory lua5.2/ubuntu_1204_x86 --file build_lua5.2_ubuntu_1204_x86.tar.gz --gzip
-tar --extract --directory lua5.3/ubuntu_1204_x86 --file build_lua5.3_ubuntu_1204_x86.tar.gz --gzip
-
-tar --extract --directory lua5.1/ubuntu_1204_x86_64 --file build_lua5.1_ubuntu_1204_x86_64.tar.gz --gzip
-tar --extract --directory lua5.2/ubuntu_1204_x86_64 --file build_lua5.2_ubuntu_1204_x86_64.tar.gz --gzip
-tar --extract --directory lua5.3/ubuntu_1204_x86_64 --file build_lua5.3_ubuntu_1204_x86_64.tar.gz --gzip
 
 tar --extract --directory lua5.1/ubuntu_1404_x86 --file build_lua5.1_ubuntu_1404_x86.tar.gz --gzip
 tar --extract --directory lua5.2/ubuntu_1404_x86 --file build_lua5.2_ubuntu_1404_x86.tar.gz --gzip
@@ -89,6 +81,14 @@ tar --extract --directory lua5.1/ubuntu_1610_x86_64 --file build_lua5.1_ubuntu_1
 tar --extract --directory lua5.2/ubuntu_1610_x86_64 --file build_lua5.2_ubuntu_1610_x86_64.tar.gz --gzip
 tar --extract --directory lua5.3/ubuntu_1610_x86_64 --file build_lua5.3_ubuntu_1610_x86_64.tar.gz --gzip
 
+tar --extract --directory lua5.1/ubuntu_1704_x86 --file build_lua5.1_ubuntu_1704_x86.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86 --file build_lua5.2_ubuntu_1704_x86.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86 --file build_lua5.3_ubuntu_1704_x86.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1704_x86_64 --file build_lua5.1_ubuntu_1704_x86_64.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86_64 --file build_lua5.2_ubuntu_1704_x86_64.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86_64 --file build_lua5.3_ubuntu_1704_x86_64.tar.gz --gzip
+
 popd
 
 
@@ -96,16 +96,22 @@ pushd build/org.muhkuh.lua-lua5.1-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.1
 make
 make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.1-romloader-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.1-romloader-*.tar.xz
 popd
 
 pushd build/org.muhkuh.lua-lua5.2-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.2
 make
 make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.2-romloader-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.2-romloader-*.tar.xz
 popd
 
 pushd build/org.muhkuh.lua-lua5.3-romloader
 cmake -DCMAKE_INSTALL_PREFIX="" ../../jonchki/installer/lua5.3
 make
 make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.3-romloader-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/org/muhkuh/lua/romloader/*/lua5.3-romloader-*.tar.xz
 popd
