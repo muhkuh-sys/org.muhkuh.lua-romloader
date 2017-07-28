97d2ee628bd7e495f350dea0404c056b2b6cafad
dizzydevil <stephan_lesch@users.sourceforge.net>
2017-07-28 18:11:22 +0200

    add interface config for Olimex ARM USB Tiny H, add debug output

--- external/openocd/jtag_detect_init.tcl
+++ external/openocd/jtag_detect_init.tcl
@@ -1,4 +1,4 @@
-puts "Hello world (tcl)"
+puts "loading script jtag_detect_init.tcl"
 
 # Configure an NXHX JTAG interface.
 proc setup_interface_nxhx {strBoardName} {
@@ -21,9 +21,26 @@ proc setup_interface_nxjtag_usb {} {
 	ftdi_vid_pid 0x1939 0x0023
 	adapter_khz 100
 
+	#ftdi_layout_init 0x0308 0x030b
+	#ftdi_layout_signal nTRST -data 0x0100
+	#ftdi_layout_signal nSRST -data 0x0200 
+
 	ftdi_layout_init 0x0308 0x030b
 	ftdi_layout_signal nTRST -data 0x0100 -oe 0x0100
 	ftdi_layout_signal nSRST -data 0x0200 -oe 0x0200
+}
+
+proc setup_interface_olimex_arm_usb_tiny_h {} {
+	interface ftdi
+	transport select jtag
+	ftdi_device_desc "Olimex OpenOCD JTAG ARM-USB-TINY-H"
+	ftdi_vid_pid 0x15ba 0x002a
+	adapter_khz 100
+	
+	ftdi_layout_init 0x0808 0x0a1b
+	ftdi_layout_signal nSRST -oe 0x0200
+	ftdi_layout_signal nTRST -data 0x0100 -oe 0x0100
+	ftdi_layout_signal LED -data 0x0800  
 }
 
 proc setup_interface_jtagkey {} {
@@ -42,6 +59,7 @@ proc setup_interface_jtagkey {} {
 # Configure an interface.
 # todo: add Olimex?
 proc setup_interface {strInterfaceID} {
+	echo "+setup_interface $strInterfaceID"
 	if {$strInterfaceID == "NXJTAG-USB" } {
 		setup_interface_nxjtag_usb
 		
@@ -58,38 +76,49 @@ proc setup_interface {strInterfaceID} {
 		setup_interface_nxhx "NXHX 500-ETM"
 	} 
 	
+	echo "-setup_interface $strInterfaceID"
 }
 
 
 proc probe_interface {} {
+	echo "+probe_interface"
 	set RESULT -1
 	
 	if {[ catch {jtag init} ]==0 } {
 			set RESULT {OK}
 	}
+	echo "-probe_interface $RESULT"
 	return $RESULT
 }
 
 
 
-
 proc probe_cpu {strCpuID} {
+	echo "+probe_cpu $strCpuID"
 	global SC_CFG_RESULT
 	set SC_CFG_RESULT 0
 	
 	if { $strCpuID == "netX_ARM966" } {
-		
+		# During the detection phase_ the setup event is triggered when the JTAG chain is successfully verified.
+		# Then the target is created, which is not really needed in this moment. The reset-init event is not triggered.
+		# When a connection to this interface/device is created, the reset-init event is triggered.
+		# The CPU is halted after reset init is called, but before the reset-init event handler is called.
 		jtag newtap netX_ARM966 cpu -irlen 4 -ircapture 1 -irmask 0xf -expected-id 0x25966021
-		jtag configure netX_ARM966.cpu -event setup { global SC_CFG_RESULT ; echo {Yay} ; set SC_CFG_RESULT {OK} }
+		jtag configure netX_ARM966.cpu -event setup { global SC_CFG_RESULT ; echo {Yay - setup ARM966} ; set SC_CFG_RESULT {OK} }
+		echo "+jtag init"
 		jtag init
+		echo "-jtag init"
 		
 		if { $SC_CFG_RESULT=={OK} } {
+			echo {creating target netX_ARM966.cpu}
 			target create netX_ARM966.cpu arm966e -endian little -chain-position netX_ARM966.cpu
-			netX_ARM966.cpu configure -event reset-init { halt }
+			#this halt might not really do anything, the cpu is halted before the message appears .
+			netX_ARM966.cpu configure -event reset-init { echo {netX_ARM966.cpu  reset-init event}; halt } 
 			netX_ARM966.cpu configure -work-area-phys 0x0380 -work-area-size 0x0080 -work-area-backup 1
 			
 			global strTarget
 			set strTarget ARM966
+			echo {Done creating target netX_ARM966.cpu}
 		}
 		
 	
@@ -145,27 +174,34 @@ proc probe_cpu {strCpuID} {
 		
 		
 	}
-	
+	echo "- probe_cpu $SC_CFG_RESULT"
 	return $SC_CFG_RESULT
 }
 
 # todo: pass target name from plugin 
 proc reset_board {} {
 	global strTarget
+	echo "+ reset_board  target: $strTarget"
 	if { $strTarget == "netX4000" } then {
 		reset_netx4000
 	} else {
 		reset_netX_ARM926_ARM966
 	}
+	echo "- reset_board "
 }
 
 proc reset_netX_ARM926_ARM966 {} {
 	reset_config trst_and_srst
+	#reset_config none separate
 	adapter_nsrst_delay 500
 	jtag_ntrst_delay 500
 	
+	echo {+init}
 	init
+	echo {-init}
+	echo {+reset init}
 	reset init
+	echo {-reset init}
 }
 
 
@@ -259,3 +295,5 @@ proc reset_netx4000 {} {
 	
 	puts "-reset_netx4000"
 }
+
+echo "Done loading jtag_detect_init.tcl"
