f3c36e0e3c7257c6f819404904b54021ad9b17d6
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-09-20 16:17:37 +0200

    Install the complete plugins folder instead of single binaries.

--- jonchki/installer/lua5.3/install.lua
+++ jonchki/installer/lua5.3/install.lua
@@ -4,86 +4,38 @@ local tResult
 
 if strDistId=='windows' then
   if strCpuArch=='x86' then
-    t:install('windows_x86/lua_plugins/romloader.dll',                       '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/romloader_eth.dll',                   '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/romloader_jtag.dll',                  '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/openocd.dll',                         '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/romloader_papa_schlumpf.dll',         '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/romloader_uart.dll',                  '${install_lua_cpath}/')
-    t:install('windows_x86/lua_plugins/romloader_usb.dll',                   '${install_lua_cpath}/')
+    t:install('windows_x86/lua_plugins/',                       '${install_lua_cpath}/')
     tResult = true
   elseif strCpuArch=='x86_64' then
-    t:install('windows_x86_64/lua_plugins/romloader.dll',                    '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/romloader_eth.dll',                '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/romloader_jtag.dll',               '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/openocd.dll',                      '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/romloader_papa_schlumpf.dll',      '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/romloader_uart.dll',               '${install_lua_cpath}/')
-    t:install('windows_x86_64/lua_plugins/romloader_usb.dll',                '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua_plugins/',                    '${install_lua_cpath}/')
     tResult = true
   end
 
 elseif strDistId=='ubuntu' then
   if strDistVersion=='14.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1404_x86/lua_plugins/romloader.so',                  '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/romloader_eth.so',              '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/romloader_jtag.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/openocd.so',                    '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/romloader_papa_schlumpf.so',    '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/romloader_uart.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86/lua_plugins/romloader_usb.so',              '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua_plugins/',                 '${install_lua_cpath}/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/openocd.so',                 '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1404_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/',              '${install_lua_cpath}/')
       tResult = true
     end
 
   elseif strDistVersion=='16.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1604_x86/lua_plugins/romloader.so',                  '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/romloader_eth.so',              '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/romloader_jtag.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/openocd.so',                    '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/romloader_papa_schlumpf.so',    '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/romloader_uart.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86/lua_plugins/romloader_usb.so',              '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua_plugins/',                 '${install_lua_cpath}/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/openocd.so',                 '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1604_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/',              '${install_lua_cpath}/')
       tResult = true
     end
 
   elseif strDistVersion=='17.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1704_x86/lua_plugins/romloader.so',                  '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/romloader_eth.so',              '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/romloader_jtag.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/openocd.so',                    '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/romloader_papa_schlumpf.so',    '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/romloader_uart.so',             '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86/lua_plugins/romloader_usb.so',              '${install_lua_cpath}/')
+      t:install('ubuntu_1704_x86/lua_plugins/',                 '${install_lua_cpath}/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader.so',               '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader_eth.so',           '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader_jtag.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/openocd.so',                 '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader_papa_schlumpf.so', '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader_uart.so',          '${install_lua_cpath}/')
-      t:install('ubuntu_1704_x86_64/lua_plugins/romloader_usb.so',           '${install_lua_cpath}/')
+      t:install('ubuntu_1704_x86_64/lua_plugins/',              '${install_lua_cpath}/')
       tResult = true
     end
   end
