2348cfeeec43e5df838786402423357813fb6851
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-25 17:46:25 +0200

    Implement the Swig runtime generator as a custom target.

--- cmake/muhkuh_swig_lua_runtime.cmake
+++ cmake/muhkuh_swig_lua_runtime.cmake
@@ -1,6 +1,6 @@
 #----------------------------------------------------------------------------
 #
-# Add a custom command and a custom target for the swig runtime includes.
+# Add a custom target for the swig runtime includes.
 # The runtimes are generated during the build process by running the swig
 # executable.
 #
@@ -10,13 +10,9 @@ SET(SWIG_RUNTIME_OUTPUT_PATH ${CMAKE_BINARY_DIR}/swig_runtime)
 MAKE_DIRECTORY(${SWIG_RUNTIME_OUTPUT_PATH})
 
 # TODO: Replace this and all other custom commands with ExternalProject ?
-ADD_CUSTOM_COMMAND(
-	OUTPUT ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
-	COMMAND "${SWIG_EXECUTABLE}" -lua -external-runtime ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
-	COMMENT "Build the swig lua runtime header."
-	VERBATIM
+ADD_CUSTOM_TARGET(TARGET_swigluarun ALL
+                  COMMAND "${SWIG_EXECUTABLE}" -lua -external-runtime ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
+                  COMMENT "Build the swig lua runtime header."
+                  BYPRODUCTS ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h
+                  VERBATIM
 )
-
-ADD_CUSTOM_TARGET(swigluarun DEPENDS ${SWIG_RUNTIME_OUTPUT_PATH}/swigluarun.h)
-
-
