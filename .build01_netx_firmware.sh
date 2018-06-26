c650a5db2ef55f88d0f947cdf9e0bb7ffe2804e0
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-26 15:45:44 +0200

    Remove obsolete files.

--- .build01_netx_firmware.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-#-----------------------------------------------------------------------------
-#
-# Build the netX firmware versions.
-#
-python2.7 mbs/mbs
--- .local.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-export PATH=/usr/mingw-w64-i686/bin:/usr/mingw-w64-x86_64/bin:${PATH}
