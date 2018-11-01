730a792ecb341edbb86b87005700f9edcb76bebf
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-11-01 14:32:27 +0100

    Build the test code for the netX90 FULL.

--- plugins/romloader/_test/testcode/src/main.h
+++ plugins/romloader/_test/testcode/src/main.h
@@ -2,7 +2,20 @@
 #ifndef __main__
 #define __main__
 
-/* insert some definitions here */
+
+typedef enum
+{
+	NETX_CONSOLEAPP_RESULT_OK = 0,
+	NETX_CONSOLEAPP_RESULT_ERROR = 1
+} NETX_CONSOLEAPP_RESULT_T;
+
+typedef struct
+{
+	unsigned long ulReturnValue;
+	void *pvInitParams;
+	void *pvReturnMessage;
+} NETX_CONSOLEAPP_PARAMETER_T;
+
 
 #endif
 
