b25eac4c7b2fb28398b22d97c8c230c8e7a7b4ce
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-09-19 16:38:10 +0200

    Update jonchki to v0.0.4.1 .

--- build_artifact.py
+++ build_artifact.py
@@ -59,7 +59,7 @@ strCfg_jonchkiHerePath = os.path.join(
     'jonchki'
 )
 # This is the Jonchki version to use.
-strCfg_jonchkiVersion = '0.0.3.1'
+strCfg_jonchkiVersion = '0.0.4.1'
 # Look in this folder for Jonchki archives before downloading them.
 strCfg_jonchkiLocalArchives = os.path.join(
     strCfg_projectFolder,
