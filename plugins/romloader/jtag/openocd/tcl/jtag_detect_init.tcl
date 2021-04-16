b834eb92c52492cdbad39fc9e052185a568bb286
dizzydevil <stephan_lesch@users.sourceforge.net>
2021-04-16 13:16:05 +0200

    [feature] Add Support for NXEB 90-SPE. (NXTFLASHER-229)

--- plugins/romloader/jtag/openocd/tcl/jtag_detect_init.tcl
+++ plugins/romloader/jtag/openocd/tcl/jtag_detect_init.tcl
@@ -88,6 +88,18 @@ proc setup_interface_nxhx90_mc {strLocation} {
 	ftdi_layout_signal nSRST -data 0x0200 -oe 0x0200 
 }
 
+proc setup_interface_nxeb_90_spe {strLocation} {
+	interface ftdi
+	ftdi_location $strLocation
+	transport select jtag
+	# ftdi_device_desc "NXEB 90-SPE"
+	ftdi_vid_pid 0x1939 0x0032
+	adapter_khz 1000
+	
+	ftdi_layout_init 0x0308 0x030b
+	ftdi_layout_signal nTRST -data 0x0100 -oe 0x0100
+	ftdi_layout_signal nSRST -data 0x0200 -oe 0x0200 
+}
 
 proc setup_interface_nrpeb_h90_re {strLocation} {
 	interface ftdi
@@ -165,6 +177,7 @@ proc setup_interface {strInterfaceID strLocation} {
 	} elseif {$strInterfaceID == "NXHX_500_50_51_10"}     {setup_interface_nxhx_generic $strLocation
 	} elseif {$strInterfaceID == "NXHX_90-JTAG"}          {setup_interface_nxhx90_jtag $strLocation
 	} elseif {$strInterfaceID == "NXHX_90-MC"}            {setup_interface_nxhx90_mc $strLocation
+	} elseif {$strInterfaceID == "NXEB_90-SPE"}           {setup_interface_nxeb90_spe $strLocation
 	} elseif {$strInterfaceID == "NXJTAG-4000-USB"}       {setup_interface_nxjtag_4000_usb $strLocation
 	} elseif {$strInterfaceID == "NRPEB_H90-RE"}          {setup_interface_nrpeb_h90_re $strLocation
 	}
