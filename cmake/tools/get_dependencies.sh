b3cd73254d8ef06c8fe9814c59df3d178938d1f7
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-08-30 11:03:29 +0200

    Remove files from local package installation which are conflicting with the cross-compiler.

--- cmake/tools/get_dependencies.sh
+++ cmake/tools/get_dependencies.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 PKG_DIR=$(pwd)
-apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $1 | grep "^\w")
+apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends $@ | grep "^\w")
 find . -name "*.deb" -exec dpkg-deb --extract '{}' . \;
 ALLLINKS=$(find . -xtype l)
 for LINK in ${ALLLINKS}
@@ -12,3 +12,12 @@ do
 		ln -fs ${PKG_DIR}${TARGET} ${LINK}
 	fi
 done
+
+# Remove any local installed versions of libc and libpthread. These libs are
+# shipped with the compiler. Local versions in the "packages" folder might
+# conflict with the compiler builtins.
+find . -name "libc[._]*" -delete
+find . -name "libpthread.*" -delete
+find . -name "stdio.h" -delete
+find . -name "math.h" -delete
+find . -name "signal.h" -delete
