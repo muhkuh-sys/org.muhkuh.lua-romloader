d0157033a969dc81acaf4b6f382867415a19e92e
fwoermann <fwoermann@hilscher.com>
2024-01-11 11:09:44 +0100

    [code] remove experimental function

--- plugins/romloader/romloader.cpp
+++ plugins/romloader/romloader.cpp
@@ -2533,32 +2533,3 @@ const char *romloader::get_error_message(TRANSPORTSTATUS_T tStatus)
 	return pcMessage;
 }
 
-
-bool romloader::is_monitor_packet_typ(MONITOR_PACKET_TYP_T tPacketTyp)
-{
-	bool fResult;
-	fResult = false;
-	switch(tPacketTyp)
-	{
-		case MONITOR_PACKET_TYP_CommandRead:
-		case MONITOR_PACKET_TYP_CommandWrite:
-		case MONITOR_PACKET_TYP_CommandExecute:
-		case MONITOR_PACKET_TYP_Status:
-		case MONITOR_PACKET_TYP_ReadData:
-		case MONITOR_PACKET_TYP_CallMessage:
-		case MONITOR_PACKET_TYP_ACK:
-		case MONITOR_PACKET_TYP_CancelOperation:
-		case MONITOR_PACKET_TYP_Command_Start_Hboot:
-		case MONITOR_PACKET_TYP_Command_Info:
-		case MONITOR_PACKET_TYP_Info_Data:
-		case MONITOR_PACKET_TYP_Command_Start_USIP:
-		case MONITOR_PACKET_TYP_MagicData:
-		case MONITOR_PACKET_TYP_CommandMagic:
-			fResult = true;
-			break;
-		default: 
-			fResult = false;
-			break;
-	}
-	return fResult
-}
